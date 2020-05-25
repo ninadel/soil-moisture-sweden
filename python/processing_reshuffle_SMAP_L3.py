a9b1c9bdaf8c2358db39bdf22f087153b1178305
Nina Del Rosario <delrosan@oregonstate.edu>
2020-05-24 19:04:26 -0600

    cleaning up SMAP L3 processing reshuffle script

--- python/processing_reshuffle_SMAP_L3.py
+++ python/processing_reshuffle_SMAP_L3.py
@@ -5,6 +5,8 @@ import os
 from datetime import datetime
 from smap_io.interface import SPL3SMP_Ds, SMAPTs
 
+reshuffle = True
+test_reshuffle = False
 
 input_root = r'C:\git\soil-moisture-sweden\sm_sample_files\SPL3SMP-smap-l3-36km\20180601_20180630'
 outputpath = r'C:\git\soil-moisture-sweden\test_output_data\test_smapL3_reshuffle'
@@ -18,14 +20,15 @@ input_dataset = SPL3SMP_Ds(input_root, parameter=parameters,
                                crid=None, flatten=True)
 
 # reshuffle
-smap_L3_reshuffle.reshuffle(input_dataset, outputpath, startdate, enddate, parameters, overpass='AM')
+if reshuffle:
+    smap_L3_reshuffle.reshuffle(input_dataset, outputpath, startdate, enddate, parameters, overpass='AM')
 
 # test reshuffle
 # output note: why is timestamp set to midnight?
-# ds = SMAPTs(outputpath, parameters=['soil_moisture','soil_moisture_error', 'retrieval_qual_flag', 'surface_flag'], ioclass_kws={'read_bulk': True})
-# ds = SMAPTs(outputpath, parameters=['retrieval_qual_flag', 'surface_flag'],
-#             ioclass_kws={'read_bulk': True})
-# read_ts takes either lon, lat coordinates or a grid point indices.
-# and returns a pandas.DataFrame
-# ts = ds.read(19.556539, 64.182029) # (lon, lat)
-# print(ts.head())
+if test_reshuffle:
+    ds = SMAPTs(outputpath, parameters=['soil_moisture','soil_moisture_error', 'retrieval_qual_flag', 'surface_flag'],
+                ioclass_kws={'read_bulk': True})
+    # read_ts takes either lon, lat coordinates or a grid point indices.
+    # and returns a pandas.DataFrame
+    ts = ds.read(19.556539, 64.182029) # (lon, lat)
+    print(ts.head())
