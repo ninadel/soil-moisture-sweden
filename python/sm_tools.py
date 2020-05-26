6d15c59da88dd9bd0a3bf8eb6077b10e280e9bcd
Nina Del Rosario <delrosan@oregonstate.edu>
2020-05-25 20:23:13 -0600

    moved some functions to dataset.py classes

--- python/sm_tools.py
+++ python/sm_tools.py
@@ -1,5 +1,6 @@
 import ismn
 import pandas
+import pytesmo
 
 
 # based on a directory of in situ text files, create a dictionary of stations
@@ -8,71 +9,23 @@ def get_station_dict(input_dir):
     # see "C:\git\nordic-insitu-sm-data\ismn-network-processing.ipynb" for reference
     pass
 
-
-# get data for all network/stations in a dictionary
-def get_global_data(dict, ts):
-    # start global dataframe
-    # get networks from dict
-    # for each network
-    # get network data
-    # add network column
-    # concat to global dataframe
-    # get network data
-    # add network column
-    # concat network dataframes
-    pass
-
-
-# get network data (network, ts)
-def get_network_data(network, ts):
-    # start network dataframe
-    # get stations from dict
-    # for each station
-    # get station data
-    # add station column
-    # concat to network dataframe
-    pass
-
-
-# get station data (station, ts)
-def get_station_data(station, ts):
-    # get lon and lat from dictionary
-    # get location data (lon = station['lon'], lat = station['lat', ts)
-    pass
-
-
 # get location data (lon, lat, ts)
 def get_location_data(lon, lat, ts):
     # get data
     # return dataframe
     pass
 
-
-def get_filtered_data(matched_data, network_filter=None, station_filter=None, year_filter=None, month_filter=None):
-    """
-    network_filter = list of networks
-    station_filter = list of stations
-    year_filter = list of years
-    month_filter = list of months
-    """
-
-    if network_filter is not None:
-        # filter based on network
-        pass
-    if station_filter is not None:
-        # filter based on station
-        pass
-    if year_filter is not None:
-        # filter based on year
-        pass
-    if month_filter is not None:
-        # filter based on month
-        pass
-    pass
+# get data for all network/stations in a dictionary
+def compare_datasets(eval_dataset, ref_dataset, timeframe=None):
+    """""
+    eval_dataset: dataset of product to be evaluated
+    ref_dataset: reference dataset
+    timeframe: timeframe dictionary, default is None
+    """""
+    # initiate matched dataset object
+    matched_data = []
+    # get metrics on matched datset object
+    metrics = matched_data.get_metrics()
+    return metrics
 
 
-def get_metrics(dataset):
-    # dataset should have 3 columns: timestamp, data 1, data 2
-    # compare both columns to get metrics
-    # return list of metrics
-    pass
