343d896c50b0dce57ef65e20f8de75112d2079d2
Nina Del Rosario <delrosan@oregonstate.edu>
2020-05-28 13:55:54 -0600

    adding dataset dict to 12.5 TS dictionary

--- python/read_ASCAT_12-5_TS.py
+++ python/read_ASCAT_12-5_TS.py
@@ -1,7 +1,7 @@
 import os
 import numpy
 import json
-from ascat.h_saf import H25Ts, H109Ts, H111Ts, H113Ts
+from ascat.h_saf import H25Ts, H109Ts, H111Ts, H113Ts, H115Ts
 from datetime import datetime
 
 # H25 Metop ASCAT DR2015 SSM 12.5 km sampling
@@ -11,6 +11,8 @@ from datetime import datetime
 # H112 Metop ASCAT DR2017 EXT SSM time series 12.5 km sampling
 # H113 Metop ASCAT DT2018 SSM time series 12.5 Km sampling
 # H114 Metop ASCAT DR2018 EXT time series 12.5 Km
+# H115 Metop ASCAT DT2019 SSM time series 12.5 Km sampling
+# H116 Metop ASCAT DR2019 EXT time series 12.5 Km
 
 # The soil moisture product represents the water content in the upper soil layer (< 2 cm) in relative units between
 # totally dry conditions (0%) and total water capacity (100%). The time series are available on a discrete global grid
@@ -19,32 +21,45 @@ from datetime import datetime
 # uncertainty. The soil moisture product has not been pre-filtered, meaning that a masking of invalid measurements
 # (e.g. frozen ground, snow cover) by the user is highly recommended before further processing.
 
-# load station_dict from external file
-with open('station_dict.json', 'r') as f:
-    station_dict = json.load(f)
+# dictionary for dataset parameters, for each reader in this dictionary, make sure the class is imported
+datasets_dict = {'ASCAT 12.5 TS' :
+    {
+        'ts_dir': r'C:\git\soil-moisture-sweden\sm_sample_files\ascat-h115-ts-2019',
+        'grid_dir': r'C:\git\soil-moisture-sweden\ascat_ts_aux\warp5_grid',
+        'grid_file': 'TUW_WARP5_grid_info_2_3.nc',
+        'static_layers_dir': r'C:\git\soil-moisture-sweden\sm_sample_files\h-saf_static_layers\static_layer',
+        'reader_name': 'ascat_12-5_ts',
+        'reader_class': 'H115Ts'
+    }
+}
 
-print(station_dict)
+# load networks_dict from external file
+with open('networks_dict.json', 'r') as f:
+    networks_dict = json.load(f)
 
-test_station = station_dict['FMI']['SAA111']
+print(networks_dict)
+
+test_station = networks_dict['FMI']['SAA111']
 print(test_station)
 print(test_station['lon'], test_station['lat'])
 
-# is this optional?
-static_layers_dir = r'C:\git\soil-moisture-sweden\sm_sample_files\h-saf_static_layers\static_layer'
-# update to 2.2?
-grid_dir = r'C:\git\soil-moisture-sweden\sm_sample_files\TUW_WARP5_grid_info_2_1'
-# update to 2.2?
-grid_file = 'TUW_WARP5_grid_info_2_1.nc'
+for dataset, dataset_dict in datasets_dict.items():
+    dataset_name = dataset
+    print(dataset)
+    # is this optional?
+    static_layers_dir = dataset_dict['static_layers_dir']
+    # update to 2.2?
+    grid_dir = dataset_dict['grid_dir']
+    # update to 2.2?
+    grid_file = dataset_dict['grid_file']
 
-ts_dir = r'C:\git\soil-moisture-sweden\sm_sample_files\ascat-h113-ts-2018'
+    ts_dir = dataset_dict['ts_dir']
 
-ts_reader = H113Ts(ts_dir,
-                             grid_dir,
-                             grid_filename=grid_file,
-                             static_layer_path=static_layers_dir)
+    ts_reader = eval(dataset_dict['reader_class']+'(ts_dir, grid_dir, grid_filename=grid_file, static_layer_path=static_layers_dir)')
 
+    ts = ts_reader.read(test_station['lon'], test_station['lat'])
 
-ts = ts_reader.read(test_station['lon'], test_station['lat'])
-
-print(ts)
+    print(ts.data.columns)
+    print(ts.data['sm'])
+    # help(ts)
 
