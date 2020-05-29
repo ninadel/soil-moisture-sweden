f5b1c72100b235e41f413e2570e590702ba4a39c
Nina Del Rosario <delrosan@oregonstate.edu>
2020-05-28 23:04:01 -0600

    trying to run SMAP L4 reshuffle but resulting in error

--- python/processing_reshuffle_SMAP_L4.py
+++ python/processing_reshuffle_SMAP_L4.py
@@ -1,33 +1,31 @@
 import os
-import smap_L3_reshuffle
+import reshuffle_smap_L4
 # from smap_io import reshuffle
 import os
 from datetime import datetime
-from smap_io.interface import SPL3SMP_Ds, SMAPTs
+from smap_extension import SPL4SMP_nc_Ds
+from smap_io import SMAPTs
 
 reshuffle = True
 test_reshuffle = False
 
-input_root = r'C:\git\soil-moisture-sweden\sm_sample_files\SPL3SMP-smap-l3-36km\20180601_20180630'
-outputpath = r'C:\git\soil-moisture-sweden\test_output_data\test_smapL3_reshuffle'
+input_root = r'C:\git\soil-moisture-sweden\sm_sample_files\SPL4SMAU-smap-ma-l4-09km_clipped_nc'
+outputpath = r'C:\git\soil-moisture-sweden\test_output_data\test_smapL4_reshuffle'
 startdate = datetime(2018, 6, 1, 0, 0, 0)
 enddate = datetime(2018, 6, 30, 23, 59, 59)
-parameters = ['soil_moisture', 'soil_moisture_error', 'retrieval_qual_flag', 'surface_flag']
+parameters = ['sm_surface_analysis', 'sm_surface_analysis_ensstd']
 
-# initialize SMAP L3 reader
-input_dataset = SPL3SMP_Ds(input_root, parameter=parameters,
-                               overpass='AM', subpath_templ = [],
-                               crid=None, flatten=True)
+# initialize SMAP L4 reader
+input_dataset = SPL4SMP_nc_Ds(input_root, parameter=parameters, subpath_templ=[], flatten=True)
 
 # reshuffle
 if reshuffle:
-    smap_L3_reshuffle.reshuffle(input_dataset, outputpath, startdate, enddate, parameters, overpass='AM')
+    reshuffle_smap_L4.reshuffle(input_dataset, outputpath, startdate, enddate, parameters)
 
 # test reshuffle
 # output note: why is timestamp set to midnight?
 if test_reshuffle:
-    ds = SMAPTs(outputpath, parameters=['soil_moisture','soil_moisture_error', 'retrieval_qual_flag', 'surface_flag'],
-                ioclass_kws={'read_bulk': True})
+    ds = SMAPTs(outputpath, parameters=parameters, ioclass_kws={'read_bulk': True})
     # read_ts takes either lon, lat coordinates or a grid point indices.
     # and returns a pandas.DataFrame
     ts = ds.read(19.556539, 64.182029) # (lon, lat)
