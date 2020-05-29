4dfb9e823774f9b8428132501f8327e8054023ba
Nina Del Rosario <delrosan@oregonstate.edu>
2020-05-28 20:21:51 -0600

    adding for loop

--- python/read_GLDASts.py
+++ python/read_GLDASts.py
@@ -4,26 +4,45 @@ import json
 from gldas.interface import GLDASTs
 from datetime import datetime
 
+# dictionary for dataset parameters, for each reader in this dictionary, make sure the class is imported
+datasets_dict = {'GLDAS' :
+    {
+        'ts_dir': r'C:\git\soil-moisture-sweden\test_output_data\test_gldas_reshuffle',
+        'grid_dir': r'C:\git\soil-moisture-sweden\test_output_data\test_gldas_reshuffle',
+        'grid_file': 'grid.nc',
+        'static_layers_dir': None,
+        'reader_name': 'GLDAS_ts',
+        'reader_class': 'GLDASTs(ts_dir)'
+    }
+}
+
 # load station_dict from external file
-with open('station_dict.json', 'r') as f:
-    station_dict = json.load(f)
+with open('networks_dict.json', 'r') as f:
+    networks_dict = json.load(f)
 
-print(station_dict)
+print(networks_dict)
 
-test_station = station_dict['FMI']['SAA111']
+test_station = networks_dict['FMI']['SAA111']
 print(test_station)
 print(test_station['lon'], test_station['lat'])
 
-# is this optional?
-static_layer_path = None
-grid_dir = r'C:\git\soil-moisture-sweden\gldas_ts_aux'
-grid_file = 'grid.nc'
+for dataset, dataset_dict in datasets_dict.items():
+    dataset_name = dataset
+    print(dataset)
+    # is this optional?
+    static_layers_dir = dataset_dict['static_layers_dir']
+    # update to 2.2?
+    grid_dir = dataset_dict['grid_dir']
+    # update to 2.2?
+    grid_file = dataset_dict['grid_file']
 
-ts_dir = r'C:\git\soil-moisture-sweden\test_output_data\test_gldas_reshuffle'
-ts_reader = ds = GLDASTs(ts_dir)
+    ts_dir = dataset_dict['ts_dir']
 
-ts = ts_reader.read(test_station['lon'], test_station['lat'])
+    ts_reader = eval(dataset_dict['reader_class'])
 
-print('hello')
-print(ts)
+    ts = ts_reader.read(test_station['lon'], test_station['lat'])
 
+    # print(ts.data.columns)
+    # print(ts.data['soil_moisture'])
+    print(ts)
+    print(ts.columns)
