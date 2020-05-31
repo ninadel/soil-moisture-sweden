6fce5abf4b0eee1c7c8a4303d548c1c8947ada11
Nina Del Rosario <delrosan@oregonstate.edu>
2020-05-30 22:07:40 -0600

    adding get_icos_stations and filter_icos_data functions

--- python/sm_tools.py
+++ python/sm_tools.py
@@ -4,10 +4,8 @@ Date: 5/25/2020
 Functions for analyzing soil moisture datasets
 """
 import datetime
-import ismn
 import os
 import pandas
-import pytesmo
 
 def get_timestamp():
     # Converting datetime object to string
@@ -31,6 +29,27 @@ def add_icos_data(input_dir, stations, icos_dict, data_only=True):
                 temp_dict[station] = metadata_dict
         icos_dict = temp_dict
     return icos_dict
+
+
+def get_icos_stations(dir):
+    data_dict = {}
+    for filename in os.listdir(dir):
+        filename_parts = filename.split(".")
+        station = filename_parts[0]
+        data_dict[station] = os.path.join(dir, filename)
+    return data_dict
+
+
+# takes a 2 column ICOS time series and filters based on column 2
+def filter_icos_data(data, qc_values, dropna=True):
+    qc_column = data.columns[1]
+    if type(qc_values) != list:
+        qc_values = [qc_values]
+    data = data.loc[data[qc_column].isin(qc_values)]
+    if dropna:
+        data = data.dropna()
+    return data
+
 
 # get location data (lon, lat, ts)
 def get_location_data(lon, lat, ts):
