3b0a8c2fcc9875f1c75f4593655a17590224680f
ninadel <nina.del@gmail.com>
2020-05-30 18:25:23 -0600

    collapsing years into one dataset

--- python/processing_reshuffle_GLDAS.py
+++ python/processing_reshuffle_GLDAS.py
@@ -8,34 +8,18 @@ from reshuffle_GLDAS import reshuffle
 # output folder template
 # C:\Nina_PCTower_Share\PCTower_SM\GLDAS_reshuffle\2015
 
-reshuffle = False
-
-input_root = r"C:\Users\ninad\OneDrive - Lund University\Dokument\SM_Data_ReadOnly\gldas-noah-2.1-0.25deg_global"
-output_root = r"C:\Nina_PCTower_Share\PCTower_SM\GLDAS_reshuffle"
+input_dir = r"C:\Users\ninad\OneDrive - Lund University\Dokument\SM_Data_ReadOnly\gldas-noah-2.1-0.25deg_global\GLDAS_global"
+output_dir = r"C:\Users\ninad\OneDrive - Lund University\Dokument\SM_Data_ReadOnly\gldas-noah-2.1-0.25deg_global\GLDAS_global_reshuffle"
 
 parameters = ['SoilMoi0_10cm_inst']
 
-year_strs = ['2016', '2017', '2018', '2015']
-# year_strs = ['2016', '2017', '2018']
-
-if reshuffle:
-    for year in year_strs:
-        year_int = int(year)
-        input_subdir = "GLDAS Noah 0.25 {}".format(year)
-        input_dir = os.path.join(input_root, input_subdir)
-        print(input_dir)
-        output_dir = os.path.join(output_root, year)
-        print(output_dir)
-        # assumes NC4 format
-        # moving reader from reshuffle file to this file, passing reader to reshuffle instead of dir
-        # suppressing landgrid since that seems to be out of date
-        input_dataset = GLDAS_Noah_v21_025Ds(input_dir, parameters, array_1D=True)
-        # input_dataset = GLDAS_Noah_v21_025Ds(input_root, parameters, landgrid=None, array_1D=True)
-        # for some reason I havve to manually set this if I have my files flat?
-        input_dataset.subpath_templ = []
-        startdate = datetime(year_int, 1, 1)
-        print(startdate)
-        enddate = datetime(year_int, 12, 31, 23, 59)
-        print(enddate)
-        reshuffle(input_dataset, output_dir, startdate, enddate, parameters)
+# assumes NC4 format
+# moving reader from reshuffle file to this file, passing reader to reshuffle instead of dir
+# suppressing landgrid since that seems to be out of date
+input_dataset = GLDAS_Noah_v21_025Ds(input_dir, parameters, array_1D=True)
+# input_dataset = GLDAS_Noah_v21_025Ds(input_root, parameters, landgrid=None, array_1D=True)
+# for some reason I havve to manually set this if I have my files flat?
+startdate = datetime(2015, 1, 1)
+enddate = datetime(2018, 12, 31, 23, 59)
+reshuffle(input_dataset, output_dir, startdate, enddate, parameters)
 
