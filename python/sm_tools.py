4db8f514fc3c9cddaa2a1e137f8d73f3f16784ef
Nina Del Rosario <delrosan@oregonstate.edu>
2020-06-03 16:20:04 -0600

    removing redundant function filter icos data (this is now in ISMN TS class

--- python/sm_tools.py
+++ python/sm_tools.py
@@ -24,33 +24,21 @@ def write_log(filename, string, print_string=True):
         print(string)
 
 
-def get_icos_stations(dir):
+def get_icos_stations(input_dir):
     data_dict = {}
-    for filename in os.listdir(dir):
+    for filename in os.listdir(input_dir):
         filename_parts = filename.split(".")
         station = filename_parts[0]
-        data_dict[station] = os.path.join(dir, filename)
+        data_dict[station] = os.path.join(input_dir, filename)
     return data_dict
 
 
-# REMOVE?
-# takes a 2 column ICOS time series and filters based on column 2
-def filter_icos_data(data, qc_values, dropna=True):
-    qc_column = data.columns[1]
-    if type(qc_values) != list:
-        qc_values = [qc_values]
-    data = data.loc[data[qc_column].isin(qc_values)]
-    if dropna:
-        data = data.dropna()
-    return data
-
-
-def get_product_reader(product, product_inputs, filter_data=True):
+def get_product_reader(product, product_metadata, filter_data=True):
     if product == "ASCAT 12.5 TS":
-        ts_data_dir = product_inputs["ts_data_dir"]
-        grid_dir = product_inputs["ts_data_dir"]
-        grid_file = product_inputs["grid_file"]
-        static_layers_dir = product_inputs["static_layers_dir"]
+        ts_data_dir = product_metadata["ts_dir"]
+        grid_dir = product_metadata["ts_dir"]
+        grid_file = product_metadata["grid_file"]
+        static_layers_dir = product_metadata["static_layers_dir"]
         reader = H115Ts(cdr_path=ts_data_dir, grid_path=grid_dir, grid_filename=grid_file,
                         static_layer_path=static_layers_dir)
         if filter_data:
@@ -58,12 +46,15 @@ def get_product_reader(product, product_inputs, filter_data=True):
             # filter data
             # ssf flag_values = 0, 1, 2, 3, 4
             # ssf flag_meanings = "unknown unfrozen frozen_temporary melting_water_on_the_surface permanent_ice";
-            print(product, "(filtered)", reader.data.shape)
-        return reader
+        product_data = reader.data
     if product == "GLDAS":
         pass
     if product == "SMAP L4":
         pass
+    return product_data
+
+
+def get_product_data(lon, lat, reader):
     pass
 
 
