2d946b9bede500680a9528269b21c0d4f6c77b2b
Nina Del Rosario <delrosan@oregonstate.edu>
2020-06-04 14:55:40 -0600

    adding filtering from get product reader to get product data (selfmasking adapter wasn't working)

--- python/sm_tools.py
+++ python/sm_tools.py
@@ -14,7 +14,7 @@ from pytesmo import metrics
 def get_timestamp():
     # Converting datetime object to string
     timestamp = datetime.now()
-    timestamp_str = timestamp.strftime("%Y%m%d_%H%M")
+    timestamp_str = timestamp.strftime("%Y%m%d_%H%M%S")
     return timestamp_str
 
 
@@ -34,7 +34,7 @@ def get_icos_stations(input_dir):
     return data_dict
 
 
-def get_product_reader(product, product_metadata, filter_product=True):
+def get_product_reader(product, product_metadata):
     if product == "ASCAT 12.5 TS":
         ts_dir = product_metadata["ts_dir"]
         grid_dir = product_metadata["grid_dir"]
@@ -42,8 +42,6 @@ def get_product_reader(product, product_metadata, filter_product=True):
         static_layers_dir = product_metadata["static_layers_dir"]
         reader = H115Ts(cdr_path=ts_dir, grid_path=grid_dir, grid_filename=grid_file,
                         static_layer_path=static_layers_dir)
-        if filter_product:
-            pass
     if product == "GLDAS":
         pass
     if product == "SMAP L4":
@@ -51,12 +49,18 @@ def get_product_reader(product, product_metadata, filter_product=True):
     return reader
 
 
-def get_product_data(lon, lat, product, product_metadata, product_reader, variable='sm'):
+def get_product_data(lon, lat, product, product_metadata, product_reader, filter_product=True, variable='sm'):
     ts = product_reader.read(lon, lat)
+    if product == 'ASCAT 12.5 TS':
+        data = ts.data
+        print('data pre-filter', data.shape)
+        if filter_product:
+            data = data.loc[data['ssf'] == 1]
+            data = data.loc[data['frozen_prob'] < 80]
+        print('data post-filter', data.shape)
     if variable == 'sm':
-        if product == "ASCAT 12.5 TS":
-            sm = ts.data[product_metadata['sm_field']]
-    return sm
+        data = data[product_metadata['sm_field']]
+    return data
 
 # get data for all network/stations in a dictionary
 def get_metrics(data, xcol, ycol):
