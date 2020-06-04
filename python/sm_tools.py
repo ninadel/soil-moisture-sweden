cb3af6e31aec971941508c68adb3a2b20312e36c
Nina Del Rosario <delrosan@oregonstate.edu>
2020-06-03 19:24:35 -0600

    adding get_product_data function, fixing get_product_reader func, adjusting get_metrics func

--- python/sm_tools.py
+++ python/sm_tools.py
@@ -33,33 +33,32 @@ def get_icos_stations(input_dir):
     return data_dict
 
 
-def get_product_reader(product, product_metadata, filter_data=True):
+def get_product_reader(product, product_metadata, filter_product=True):
     if product == "ASCAT 12.5 TS":
-        ts_data_dir = product_metadata["ts_dir"]
-        grid_dir = product_metadata["ts_dir"]
+        ts_dir = product_metadata["ts_dir"]
+        grid_dir = product_metadata["grid_dir"]
         grid_file = product_metadata["grid_file"]
         static_layers_dir = product_metadata["static_layers_dir"]
-        reader = H115Ts(cdr_path=ts_data_dir, grid_path=grid_dir, grid_filename=grid_file,
+        reader = H115Ts(cdr_path=ts_dir, grid_path=grid_dir, grid_filename=grid_file,
                         static_layer_path=static_layers_dir)
-        if filter_data:
-            print(product, "filter: ")
-            # filter data
-            # ssf flag_values = 0, 1, 2, 3, 4
-            # ssf flag_meanings = "unknown unfrozen frozen_temporary melting_water_on_the_surface permanent_ice";
-        product_data = reader.data
+        if filter_product:
+            pass
     if product == "GLDAS":
         pass
     if product == "SMAP L4":
         pass
-    return product_data
+    return reader
 
 
-def get_product_data(lon, lat, reader):
-    pass
-
+def get_product_data(lon, lat, product, product_metadata, product_reader, variable='sm'):
+    ts = product_reader.read(lon, lat)
+    if variable == 'sm':
+        if product == "ASCAT 12.5 TS":
+            sm = ts.data[product_metadata['sm_field']]
+    return sm
 
 # get data for all network/stations in a dictionary
-def get_metrics(data, metrics=('bias', 'rmsd', 'ubrmsd', 'pearsonr', 'pearsonr_p')):
+def get_metrics(data, xcol, ycol, metrics=('bias', 'rmsd', 'ubrmsd', 'pearsonr', 'pearsonr_p')):
     """""
     data: temporally matched dataset
     metrics: list of metrics to calculate on matched dataset
@@ -69,7 +68,7 @@ def get_metrics(data, metrics=('bias', 'rmsd', 'ubrmsd', 'pearsonr', 'pearsonr_p
         metrics = [metrics]
     # bias, rmsd, ubrmsd, pearsonr, pearsonr p-value
     metric_values = [None, None, None, None, None]
-    if data.shape[0] > 4:
+    if len(metrics) > 4:
         for metric in metrics:
             pass
     return metric_values
