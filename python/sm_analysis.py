833e3482e2f9a563f5bcd65461d9fd6d6f290daf
Nina Del Rosario <delrosan@oregonstate.edu>
2020-06-04 17:31:54 -0600

    planning

--- python/sm_analysis.py
+++ python/sm_analysis.py
@@ -4,3 +4,12 @@ Date: 5/25/2020
 Script for analyzing SM datasets
 For icos analysis see sm_analysis_icos.py
 """
+
+# function which compares reference data to evaluation data
+# reference can be either ISMN, ICOS, or GLDAS
+def evaluate(reference_type, reference_loc_dict, product_reader, startdate, enddate):
+    # ICOS: see existing dictionary
+    # ISMN: one network at a time
+    # GLDAS: one big network, prefix existing networks e.g. ICOS_Degero
+    # I
+    pass
