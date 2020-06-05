587d20c411896e15dd653f4c68bb3b4483793e36
Nina Del Rosario <delrosan@oregonstate.edu>
2020-06-04 19:22:02 -0600

    adapting to sm_config.py dataset dict

--- python/read_SMAP_L3_ts.py
+++ python/read_SMAP_L3_ts.py
@@ -8,46 +8,18 @@ import numpy
 import json
 from smap_io import SMAPTs
 from datetime import datetime
+import sm_config as config
 
 # dictionary for dataset parameters, for each reader in this dictionary, make sure the class is imported
-datasets_dict = {'SMAP L3' :
-    {
-        'ts_dir': r'C:\Nina_PCTower_Share\PCTower_SM\SPL3SMP-smap-l3-36km_nordic_reshuffle',
-        'grid_dir': r'C:\Nina_PCTower_Share\PCTower_SM\SPL3SMP-smap-l3-36km_nordic_reshuffle',
-        'grid_file': 'grid.nc',
-        'static_layers_dir': None,
-        'reader_name': 'SMAP_L3_ts',
-        'reader_class': 'SMAPTs(ts_dir)'
-    }
-}
+print_sample = False
 
-# load networks_dict from external file
-with open('networks_dict.json', 'r') as f:
-    networks_dict = json.load(f)
+ts_reader = SMAPTs(config.datasets_dict['SMAP L3']['ts_dir'])
 
-print(networks_dict)
+# Degero: 19.556539 64.182029
+ts = ts_reader.read(19.556539, 64.182029)
 
-# test_station = networks_dict['FMI']['SAA111']
-# print(test_station)
-# print(test_station['lon'], test_station['lat'])
+print(ts)
+print(ts.columns)
 
-for dataset, dataset_dict in datasets_dict.items():
-    dataset_name = dataset
-    print(dataset)
-    # is this optional?
-    static_layers_dir = dataset_dict['static_layers_dir']
-    # update to 2.2?
-    grid_dir = dataset_dict['grid_dir']
-    # update to 2.2?
-    grid_file = dataset_dict['grid_file']
-
-    ts_dir = dataset_dict['ts_dir']
-
-    ts_reader = eval(dataset_dict['reader_class'])
-
-    # Degero: 19.556539 64.182029
-    ts = ts_reader.read(19.556539, 64.182029)
-
-    print(ts)
-    print(ts.columns)
-
+if print_sample:
+    ts.to_csv('..\\product_sample_data\\SMAP-L3_sample.csv')
