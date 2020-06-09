57358fcf0128108bb632c596f6fb18d55468c7b1
Nina Del Rosario <delrosan@oregonstate.edu>
2020-06-09 16:19:03 -0600

    adding placeholders for additional products in dataset dict

--- python/sm_config.py
+++ python/sm_config.py
@@ -13,25 +13,69 @@ metrics_df_columns = ['network', 'station', 'ref_filtered', 'product', 'product_
                       'bias', 'rmsd', 'ubrmsd', 'pearsonr', 'pearsonr_p']
 
 # dictionary for dataset parameters, for each reader in this dictionary, make sure the class is imported
-datasets_dict = {'ASCAT 12.5 TS':
-    {
+datasets_dict = {
+    'ASCAT H08': {
+        'ts_dir': None,
+        'lon_field': None,
+        'lat_field': None,
+        'sm_field': None
+    },
+    'ASCAT 12.5 TS': {
         'ts_dir': r'..\input_data\ascat-h115-ts-2019',
         'grid_dir': r'..\ascat_ts_aux\warp5_grid',
         'grid_file': 'TUW_WARP5_grid_info_2_3.nc',
         'static_layers_dir': None,
+        'lon_field': None,
+        'lat_field': None,
         'sm_field': 'sm'
+    },
+    'CCI': {
+        'ts_dir': None,
+        'lon_field': None,
+        'lat_field': None,
+        'sm_field': None
     },
     'GLDAS': {
         'ts_dir': r'..\input_data\GLDAS_global_reshuffle',
+        'lon_field': None,
+        'lat_field': None,
         'sm_field': 'SoilMoi0_10cm_inst'
     },
     'SMAP L3': {
         'ts_dir': r'..\input_data\SPL3SMP-smap-l3-36km_nordic_reshuffle',
+        'lon_field': None,
+        'lat_field': None,
         'sm_field': 'soil_moisture'
+    },
+    'SMAP L3 Enhanced': {
+        'ts_dir': None,
+        'lon_field': None,
+        'lat_field': None,
+        'sm_field': None
     },
     'SMAP L4': {
         'ts_dir': r'..\input_data\SPL4SMAU_nordic_reshuffle',
+        'lon_field': None,
+        'lat_field': None,
         'sm_field': 'sm_surface_analysis'
+    },
+    'Sentinel-1': {
+        'ts_dir': None,
+        'lon_field': None,
+        'lat_field': None,
+        'sm_field': None
+    },
+    'SMOS BEC': {
+        'ts_dir': None,
+        'lon_field': None,
+        'lat_field': None,
+        'sm_field': None
+    },
+    'SMOS IC': {
+        'ts_dir': None,
+        'lon_field': None,
+        'lat_field': None,
+        'sm_field': None
     }
 }
 
