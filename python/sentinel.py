3d0579f44f3fb75b066fcacd924f3f3548105e74
Nina Del Rosario <delrosan@oregonstate.edu>
2020-06-19 10:47:02 -0600

    simplying sentinel class for now, commenting everything else out

--- python/sentinel.py
+++ python/sentinel.py
@@ -31,8 +31,9 @@ class SentinelImg(ImageBase):
     grid : pygeogrids.BasicGrid, optional (default: None)
         A grid object, on which the image data is organised. If None is passed,
     """
-    def __init__(self, filename, mode='r', parameter='ssm', flatten=False,
-                 grid=None):
+    def __init__(self, filename, mode='r', parameter='ssm'):
+    # def __init__(self, filename, mode='r', parameter='ssm', flatten=False,
+    #              grid=None):
         super(SentinelImg, self).__init__(filename, mode=mode)
 
         if type(parameter) != list:
@@ -41,7 +42,7 @@ class SentinelImg(ImageBase):
         self.flatten = flatten
 
 
-    def read(self, timestamp=None):
+    def read_img(self):
         return_data = {}
         return_meta = {}
 
@@ -77,14 +78,15 @@ class SentinelImg(ImageBase):
             return_data[parameter] = data
             return_meta[parameter] = metadata
 
-        if self.flatten:
-            longitude = longitude.flatten()
-            latitude = latitude.flatten()
-            for param in return_data.keys():
-                return_data[param] = return_data[param].flatten()
+        # if self.flatten:
+        #     longitude = longitude.flatten()
+        #     latitude = latitude.flatten()
+        #     for param in return_data.keys():
+        #         return_data[param] = return_data[param].flatten()
 
         ds.close()
-        return Image(longitude, latitude, return_data, return_meta, timestamp)
+        return return_data, return_meta
+        # return Image(longitude, latitude, return_data, return_meta, timestamp)
 
     def write(self, data):
         raise NotImplementedError()
@@ -95,90 +97,90 @@ class SentinelImg(ImageBase):
     def close(self):
         pass
 
-class SentinelDs(MultiTemporalImageBase):
-    """
-    Class for reading a collection of Sentinel-1 images in nc format.
-    Parameters
-    ----------
-    data_path: string
-        root path of the Sentinel-1 data files
-    parameter : string or list, optional
-        Default : 'ssm'
-    subpath_templ : list, optional
-        If the data is store in subpaths based on the date of the dataset then this list
-        can be used to specify the paths. Every list element specifies one path level.
-    flatten: boolean, optional
-        If true the read data will be returned as 1D arrays.
-    """
-    def __init__(self, data_path, parameter='ssm', subpath_templ=[], flatten=False):
-        ioclass_kws = {'parameter': parameter,
-                       'flatten': flatten}
-        # c_gls_SSM1km_201501010000_CEURO_S1CSAR_V1.1.1_sub.nc
-        # filename_templ = "c_gls_SSM1km_{datetime}_*.nc"
-        # datetime_format = "%Y%m%d%H%M%S" ?
-        filename_templ = "c_gls_SSM1km_{datetime}0000*.nc"
-        super(SentinelDs, self).__init__(data_path, SentinelImg, fname_templ=filename_templ, datetime_format="%Y%m%d",
-                                         subpath_templ=subpath_templ, exact_templ=False, ioclass_kws=ioclass_kws)
-
-    def tstamps_for_daterange(self, start_date, end_date):
-        """
-        return timestamps for daterange,
-        Parameters
-        ----------
-        start_date: datetime
-            start of date range
-        end_date: datetime
-            end of date range
-        Returns
-        -------
-        timestamps : list
-            list of datetime objects of each available image between
-            start_date and end_date
-        """
-
-        nxt = lambda date: date + relativedelta(days=1)
-
-        timestamps = [start_date]
-        while nxt(timestamps[-1]) <= end_date:
-            timestamps.append(nxt(timestamps[-1]))
-
-        return timestamps
-
-
-class SentinelTs(GriddedNcOrthoMultiTs):
-    def __init__(self, ts_path, grid_path=None, **kwargs):
-        '''
-        Class for reading Sentinel-1 SM time series after reshuffling.
-        Parameters
-        ----------
-        ts_path : str
-            Directory where the netcdf time series files are stored
-        grid_path : str, optional (default: None)
-            Path to grid file, that is used to organize the location of time
-            series to read. If None is passed, grid.nc is searched for in the
-            ts_path.
-        Optional keyword arguments that are passed to the Gridded Base:
-        ------------------------------------------------------------------------
-            parameters : list, optional (default: None)
-                Specific variable names to read, if None are selected, all are read.
-            offsets : dict, optional (default:None)
-                Offsets (values) that are added to the parameters (keys)
-            scale_factors : dict, optional (default:None)
-                Offset (value) that the parameters (key) is multiplied with
-            ioclass_kws: dict
-                Optional keyword arguments to pass to OrthoMultiTs class:
-                ----------------------------------------------------------------
-                    read_bulk : boolean, optional (default:False)
-                        if set to True the data of all locations is read into memory,
-                        and subsequent calls to read_ts read from the cache and not from disk
-                        this makes reading complete files faster#
-                    read_dates : boolean, optional (default:False)
-                        if false dates will not be read automatically but only on specific
-                        request useable for bulk reading because currently the netCDF
-                        num2date routine is very slow for big datasets
-        '''
-        if grid_path is None:
-            grid_path = os.path.join(ts_path, "grid.nc")
-
-        grid = load_grid(grid_path)
-        super(SentinelTs, self).__init__(ts_path, grid, **kwargs)
+# class SentinelDs(MultiTemporalImageBase):
+#     """
+#     Class for reading a collection of Sentinel-1 images in nc format.
+#     Parameters
+#     ----------
+#     data_path: string
+#         root path of the Sentinel-1 data files
+#     parameter : string or list, optional
+#         Default : 'ssm'
+#     subpath_templ : list, optional
+#         If the data is store in subpaths based on the date of the dataset then this list
+#         can be used to specify the paths. Every list element specifies one path level.
+#     flatten: boolean, optional
+#         If true the read data will be returned as 1D arrays.
+#     """
+#     def __init__(self, data_path, parameter='ssm', subpath_templ=[], flatten=False):
+#         ioclass_kws = {'parameter': parameter,
+#                        'flatten': flatten}
+#         # c_gls_SSM1km_201501010000_CEURO_S1CSAR_V1.1.1_sub.nc
+#         # filename_templ = "c_gls_SSM1km_{datetime}_*.nc"
+#         # datetime_format = "%Y%m%d%H%M%S" ?
+#         filename_templ = "c_gls_SSM1km_{datetime}0000*.nc"
+#         super(SentinelDs, self).__init__(data_path, SentinelImg, fname_templ=filename_templ, datetime_format="%Y%m%d",
+#                                          subpath_templ=subpath_templ, exact_templ=False, ioclass_kws=ioclass_kws)
+#
+#     def tstamps_for_daterange(self, start_date, end_date):
+#         """
+#         return timestamps for daterange,
+#         Parameters
+#         ----------
+#         start_date: datetime
+#             start of date range
+#         end_date: datetime
+#             end of date range
+#         Returns
+#         -------
+#         timestamps : list
+#             list of datetime objects of each available image between
+#             start_date and end_date
+#         """
+#
+#         nxt = lambda date: date + relativedelta(days=1)
+#
+#         timestamps = [start_date]
+#         while nxt(timestamps[-1]) <= end_date:
+#             timestamps.append(nxt(timestamps[-1]))
+#
+#         return timestamps
+#
+#
+# class SentinelTs(GriddedNcOrthoMultiTs):
+#     def __init__(self, ts_path, grid_path=None, **kwargs):
+#         '''
+#         Class for reading Sentinel-1 SM time series after reshuffling.
+#         Parameters
+#         ----------
+#         ts_path : str
+#             Directory where the netcdf time series files are stored
+#         grid_path : str, optional (default: None)
+#             Path to grid file, that is used to organize the location of time
+#             series to read. If None is passed, grid.nc is searched for in the
+#             ts_path.
+#         Optional keyword arguments that are passed to the Gridded Base:
+#         ------------------------------------------------------------------------
+#             parameters : list, optional (default: None)
+#                 Specific variable names to read, if None are selected, all are read.
+#             offsets : dict, optional (default:None)
+#                 Offsets (values) that are added to the parameters (keys)
+#             scale_factors : dict, optional (default:None)
+#                 Offset (value) that the parameters (key) is multiplied with
+#             ioclass_kws: dict
+#                 Optional keyword arguments to pass to OrthoMultiTs class:
+#                 ----------------------------------------------------------------
+#                     read_bulk : boolean, optional (default:False)
+#                         if set to True the data of all locations is read into memory,
+#                         and subsequent calls to read_ts read from the cache and not from disk
+#                         this makes reading complete files faster#
+#                     read_dates : boolean, optional (default:False)
+#                         if false dates will not be read automatically but only on specific
+#                         request useable for bulk reading because currently the netCDF
+#                         num2date routine is very slow for big datasets
+#         '''
+#         if grid_path is None:
+#             grid_path = os.path.join(ts_path, "grid.nc")
+#
+#         grid = load_grid(grid_path)
+#         super(SentinelTs, self).__init__(ts_path, grid, **kwargs)
