97289fc0e9424f1a1f802cfa52a78c1a362ad711
Nina Del Rosario <delrosan@oregonstate.edu>
2020-06-18 19:47:57 -0600

    cleaning up

--- python/processing_subset_GLDAS.py
+++ python/processing_subset_GLDAS.py
@@ -90,21 +90,3 @@ for file in os.listdir(input_dir):
                                fill_value=src_var_attributes['_FillValue'])
             dst[variable][:] = src[variable][:, latli:latui, lonli:lonui]
             dst[variable].setncatts(dst_var_attributes)
-
-# import netCDF4 as nc
-# toexclude = ['ExcludeVar1', 'ExcludeVar2']
-#
-# with netCDF4.Dataset("in.nc") as src, netCDF4.Dataset("out.nc", "w") as dst:
-#     # copy global attributes all at once via dictionary
-#     dst.setncatts(src.__dict__)
-#     # copy dimensions
-#     for name, dimension in src.dimensions.items():
-#         dst.createDimension(
-#             name, (len(dimension) if not dimension.isunlimited() else None))
-#     # copy all file data except for the excluded
-#     for name, variable in src.variables.items():
-#         if name not in toexclude:
-#             x = dst.createVariable(name, variable.datatype, variable.dimensions)
-#             dst[name][:] = src[name][:]
-#             # copy variable attributes all at once via dictionary
-#             dst[name].setncatts(src[name].__dict__)
