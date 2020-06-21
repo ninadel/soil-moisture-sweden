1ffd049dd632acfa9809da512a41f1cb47f53bce
ninadel <nina.del@gmail.com>
2020-06-21 17:20:29 -0600

    changing grid to ease36

--- python/reshuffle_smap_L3.py
+++ python/reshuffle_smap_L3.py
@@ -86,9 +86,9 @@ def reshuffle(input_dataset, outputpath, startdate, enddate,
     data = input_dataset.read(startdate)
     ts_attributes = data.metadata
     # ease2 grid not working
-    # ease36 = EASE2_grid(36000)
-    # lons, lats = np.meshgrid(ease36.londim, ease36.latdim)
-    grid = BasicGrid(data.lon, data.lat)
+    ease36 = EASE2_grid(36000)
+    lons, lats = np.meshgrid(ease36.londim, ease36.latdim)
+    grid = BasicGrid(lons.flatten(), lats.flatten())
 
     reshuffler = Img2Ts(input_dataset=input_dataset, outputpath=outputpath,
                         startdate=startdate, enddate=enddate, input_grid=grid,
