924df328e409ce70e5e27c35fc6e243e0b499a8f
Nina Del Rosario <delrosan@oregonstate.edu>
2020-06-24 15:56:48 -0600

    starting new classes for location and network evaluation

---
+++ python/evaluation.py
@@ -0,0 +1,59 @@
+class LocationEvaluation(object):
+    """
+    Description
+    Parameters
+    ----------
+    parameter : type
+        Description
+    Attributes
+    ----------
+    name : type
+        Description
+    """
+
+    def __init__(self, argument):
+        """
+        init method, ...
+        """
+
+    pass
+
+class LocationSetEvaluation(object):
+    """
+    Description
+    Parameters
+    ----------
+    locations : dict
+        Dictionary of locations. Each l
+    Attributes
+    ----------
+    name : type
+        Description
+    """
+
+    def __init__(self, locations, anomaly=False, timeframe=None):
+        """
+        init method, ...
+        """
+
+    pass
+
+class NetworkEvaluation(LocationSetEvaluation):
+    """
+    Description
+    Parameters
+    ----------
+    parameter : type
+        Description
+    Attributes
+    ----------
+    name : type
+        Description
+    """
+
+    def __init__(self, argument):
+        """
+        init method, ...
+        """
+
+    pass
