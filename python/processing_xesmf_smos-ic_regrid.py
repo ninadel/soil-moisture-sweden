4e1766919b3c778bfcd85543c409f3ce9847f22e
Nina del Rosario <nina.del@gmail.com>
2020-07-01 23:57:31 -0600

    added regridding, cleanup

--- python/processing_xesmf_smos-ic_regrid.py
+++ python/processing_xesmf_smos-ic_regrid.py
@@ -3,57 +3,40 @@ Author: Nina del Rosario
 Date: 6/30/2020
 Script for regridding SMOS-IC data
 """
+from datetime import datetime
 import numpy as np
 import os
+import warnings
 import xarray as xr
 import xtools
+import sm_config as config
 
+output_dir = r"../test_output_data/smos_ic_regrid_ts"
 # method options: 'nearest_s2d', 'bilinear', and 'conservative' (conservative not currently working)
 method = 'nearest_s2d'
 # method = 'bilinear'
 # method = 'conservative'
 
-try:
-    f = r"/Volumes/TOSHIBA EXT/sm_backup/smos-ic-l3-25km_global/ASC/SM_RE06_MIR_CDF3SA_20150101T000000_20150101T235959_105_001_8.DBL.nc"
-    in_dir = r"/Volumes/TOSHIBA EXT/sm_backup/smos-ic-l3-25km_global/ASC"
-    in_glob = in_dir + "/*.nc"
-    test_ds = xr.open_dataset(f)
-except:
-    f = r"D:\sm_backup\smos-ic-l3-25km_global\ASC\SM_RE06_MIR_CDF3SA_20150101T000000_20150101T235959_105_001_8.DBL.nc"
-    in_dir = r"D:\sm_backup\smos-ic-l3-25km_global\ASC"
-    in_glob = in_dir + "/*.nc"
-    test_ds = xr.open_dataset(f)
+in_dir = r"/Volumes/TOSHIBA EXT/sm_backup/smos-ic-l3-25km_global/ASC"
+in_glob = in_dir + "/*.nc"
 
 file_list = [os.path.join(in_dir, file) for file in os.listdir(in_dir)]
 # crop for testing
-file_list = file_list[0:5]
+file_list = file_list
 ds = xtools.get_mf_dataset(file_list, 'SMOS-IC')
 
 # filter out invalid data
 ds = ds.where((ds['Quality_Flag'] == 0) & (ds['Soil_Moisture'] >= 0) & (ds['Soil_Moisture'] < 1))
 
 # print summaries
-# print('ds_in summary')
-# print(ds_in.dims)
-# print(ds_in.coords)
-# print(ds_in.data_vars)
+print('ds summary')
+print(ds.dims)
+print(ds.coords)
+print(ds.data_vars)
 
-# # create shell for output grid
-# if method == 'nearest_s2d' or method == 'bilinear':
-#     ds_out = xr.Dataset({'lat': (['lat'], config.regrid_lat),
-#                          'lon': (['lon'], config.regrid_lon)})
-# elif method == 'conservative':
-#     ds_out = xr.Dataset({'lat': (['lat'], config.regrid_lat),
-#                          'lon': (['lon'], config.regrid_lon),
-#                          'lat_b': (['lat_b'], config.regrid_lat_b),
-#                          'lon_b': (['lon_b'], config.regrid_lon_b)
-#                          })
+dr = ds['Soil_Moisture']
 
-# create regridder object
-# reg = xe.Regridder(ds_in, ds_out, method)
+dr_regrid = xtools.regrid(ds, 'Soil_Moisture')
+print(dr_regrid)
 
-# regrid soil moisture
-# dr_out = reg(dr_in)
-#
-# print('dr_out')
-# print(dr_out.values)
+xtools.write_ts_quarter_deg(dr_regrid, output_dir)
