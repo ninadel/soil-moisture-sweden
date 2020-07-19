a35de0e3ee169c2ee25138745ab78fea1a68643a
Nina Del Rosario <delrosan@oregonstate.edu>
2020-07-19 15:02:15 -0600

    minor changes

--- python/processing_xarray_merge_smap_L3E.py
+++ python/processing_xarray_merge_smap_L3E.py
@@ -17,16 +17,16 @@ sm_field = config.dict_product_fields[product]['sm_field']
 output_dir = r"..\input_data\xr"
 in_dir = r"..\input_data\SPL3SMP_E_smap-L3E_09km_clipped_geographic_nc"
 
-export_ds = True
+export_ds = False
 
 # all files in directory must be product nc files
 file_list = [os.path.join(in_dir, file) for file in os.listdir(in_dir)]
 
 # open multi file dataset
 ds = xtools.get_mf_dataset(file_list, product)
+print(ds)
 
 if export_ds:
     ds.to_netcdf(os.path.join(output_dir, "smap-L3E-09km-subset-nofilter.nc"))
     print("smap-L3E-09km-subset-nofilter.nc complete")
     print(ds)
-    
