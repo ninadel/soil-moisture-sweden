04cfa313dbeaf4da51fed9e8b28f2b09cbfb4e06
Nina Del Rosario <delrosan@oregonstate.edu>
2020-07-19 16:54:21 -0600

    updating inputs

--- python/processing_xarray_export_stations_smap_L3E.py
+++ python/processing_xarray_export_stations_smap_L3E.py
@@ -12,22 +12,23 @@ import sm_config as config
 import xarray as xr
 
 output_root = r"..\test_output_data\station_csv"
-log_file = os.path.join(output_root, "log_file.csv")
 
 icos_readers = tools.get_icos_readers(config.icos_input_dir)
 ismn_readers = tools.get_ismn_readers(config.ismn_input_dir)
 # use this as a parameter below if you want to analyze both ICOS and ISMN
 station_list = icos_readers + ismn_readers
 
-product = "ERA5_0-1"
-# product = "ERA5_0-25"
+product = "SMAP L3 Enhanced"
 output_dir = os.path.join(output_root, product)
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
-f = r"..\input_data\ERA5-Land\0-1_ERA5-Land_hourly.nc"
-# f = r"..\input_data\ERA5-Land\0-25_ERA5-Land_hourly.nc"
+f = r"..\input_data\xr\smap-L3E-09km-subset-nofilter.nc"
 ds = xr.open_dataset(f)
+
+print(ds)
+print(ds.x)
+print(ds)
 
 for station in station_list:
     station_name = station.station
@@ -38,3 +39,4 @@ for station in station_list:
     ts_pandas = ts.to_pandas()
     ts_pandas.rename('swvl1', inplace=True)
     ts_pandas.to_csv(ts_file)
+    break
