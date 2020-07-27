a83e3f96bd974945eb42294b419e3f3c31a56515
Nina del Rosario <nina.del@gmail.com>
2020-07-26 21:52:56 -0600

    test script for regridding h115 rebuild data

--- python/processing_xesmf_h115_rebuild_regrid.py
+++ python/processing_xesmf_h115_rebuild_regrid.py
@@ -1,7 +1,7 @@
 """
 Author: Nina del Rosario
 Date: 7/18/2020
-Script for regridding SMAP L3 data
+Script for regridding H115 repocessed
 """
 from datetime import datetime
 import numpy as np
@@ -9,102 +9,36 @@ import os
 import warnings
 import xarray as xr
 import xtools
+import xesmf as xe
 import sm_config as config
 import matplotlib.pyplot as plt
 
 
-product = "SMAP L3"
-output_dir = r"..\input_data\xr"
-in_dir = r"..\input_data\SPL3SMP_smap-L3_36km_clipped_NoReproj_nc"
-
-export_ds = False
-
-# first file for creating lat and lon dimensions
-first_file = r"..\input_data\SPL3SMP_smap-L3_36km_clipped_NoReproj_nc\SMAP_L3_SM_P_20150401_R16510_001_HEGOUT.nc"
-first_ds = xr.open_dataset(first_file, group='Soil_Moisture_Retrieval_Data_AM')
-lon = first_ds['longitude'][0, :].values
-lat = first_ds['latitude'][:, 0].values
-local_time_utc = 5
-
-ds_list = []
-
-for filename in os.listdir(in_dir):
-    file = os.path.join(in_dir, filename)
-    datestamp = tools.get_filename_timestamp(file, r"P_[0-9]{8}_R")
-    datestamp = datestamp.replace(hour=local_time_utc)
-    print(file)
-    ds = xr.open_dataset(file, group='Soil_Moisture_Retrieval_Data_AM')
-    soil_moisture = ds['soil_moisture'].values
-    soil_moisture_da = xr.DataArray(soil_moisture, coords=[lat, lon], dims=["lat", "lon"], name="soil_moisture")
-    surface_flag = ds['surface_flag'].values
-    surface_flag_da = xr.DataArray(surface_flag, coords=[lat, lon], dims=["lat", "lon"], name="surface_flag")
-    retrieval_qual_flag = ds['retrieval_qual_flag'].values
-    retrieval_qual_flag_da = xr.DataArray(retrieval_qual_flag, coords=[lat, lon], dims=["lat", "lon"],
-                                          name="retrieval_qual_flag")
-    out_ds = xr.merge([soil_moisture_da, surface_flag_da, retrieval_qual_flag_da])
-    out_ds["time"] = datestamp
-    out_ds = out_ds.expand_dims('time').set_coords('time')
-    ds_list.append(out_ds)
-
-concat_ds = xr.concat(ds_list, dim="time")
-print(concat_ds)
-print(concat_ds.lat)
-print(concat_ds.lon)
-print(concat_ds.time)
-concat_ds.to_netcdf(os.path.join(output_dir, "smap-L3-36km-subset-filter.nc"))
-
-
-product = "SMAP L3"
-sm_field = config.dict_product_fields[product]['sm_field']
-output_dir = r"../test_output_data"
-in_dir = r"..\input_data\SPL3SMP_smap-L3_36km_clipped_geographic_nc"
-
-clean_weights = False
+product = "ASCAT 12.5 TS"
+output_dir = r"../input_data/xr"
+in_dir = r"../input_data/H115_rebuild"
+# r'/media/ninadel/TOSHIBA EXT/sample_rs_files/ascat-12.5-km-bufr'
+f = r"../input_data/H115_rebuild/test_h115_reprocessed.nc"
 regrid = True
 
-f = r"/Volumes/TOSHIBA EXT/sm_backup/xr/smap-L3-36km-subset-nofilter.nc"
 ds = xr.open_dataset(f)
 print(ds)
-dr = ds[sm_field]
-plot_test = dr.sel(time="2018-06-01T05:00:00")
-plot_test.plot()
-plt.show()
-plt.clf()
-# print(ds)
-# print(ds[sm_field].values)
 
-# filter out invalid values
-ds = ds.where((ds['retrieval_qual_flag'] == 0) | (ds['retrieval_qual_flag'] == 1) |
-              (ds['retrieval_qual_flag'] == 8))
-ds = ds.where((ds[sm_field] >= 0) & (ds[sm_field] < 1))
-dr = ds[sm_field]
-plot_test = dr.sel(time="2018-06-01T05:00:00")
-plot_test.plot()
-plt.show()
-plt.clf()
-# print(ds)
-# print(ds[sm_field].values)
-
-# if export_ds:
-#     ds.to_netcdf(os.path.join(output_dir, "smap-L3-36km-subset-nofilter.nc"))
-#     print("smap-L3-36km-subset-nofilter.nc complete")
-#     print(ds)
+print(os.listdir(in_dir))
 
 if regrid:
     print("regridding")
-    dr = ds[sm_field]
-    dr_regrid = xtools.regrid(ds, sm_field, method="nearest_s2d", reuse=False)
-    plot_test = dr_regrid.sel(time="2018-06-01T05:00:00")
-    plot_test.plot()
-    plt.show()
-    plt.clf()
-    print(dr_regrid)
-    dr_regrid_bl = xtools.regrid(ds, sm_field, method="bilinear")
-    dr_regrid[np.isnan(dr_regrid_bl)] = np.nan
-    plot_test = dr_regrid.sel(time="2018-06-01T05:00:00")
-    plot_test.plot()
-    plt.show()
-    plt.clf()
-    print(dr_regrid)
-    dr_regrid.to_netcdf(os.path.join(output_dir, "smap_L3_0-25-regrid2.nc"))
-    print("smap_L3_0-25-regrid.nc complete")
+    # dr = ds[sm_field]
+    ds_in = ds
+    ds_out = xr.Dataset({'lat': (['lat'], config.regrid_lat),
+                         'lon': (['lon'], config.regrid_lon)})
+    reg = xe.Regridder(ds_in, ds_out, method="nearest_s2d")
+    dr_in = ds_in['sm']
+    dr_out = reg(dr_in)
+    dr_out.to_netcdf(os.path.join(output_dir, "test_h115_regrid.nc"))
+    # print("smap_L3_0-25-regrid.nc complete")
+
+    #     ds_out = xr.Dataset({'lat': (['lat'], config.regrid_lat),
+    #                          'lon': (['lon'], config.regrid_lon)})
+    # reg = xe.Regridder(ds_in, ds_out, method=method, reuse_weights=reuse)
+    # dr_in = ds_in[var]
