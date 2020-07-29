9dcfb0310410a5afe7f3ef953ac85dbeea968bfc
Nina del Rosario <nina.del@gmail.com>
2020-07-29 17:21:46 -0600

    updated, now working

--- python/processing_xe_smap_L3E_regrid.py
+++ python/processing_xe_smap_L3E_regrid.py
@@ -3,42 +3,21 @@ Author: Nina del Rosario
 Date: 6/30/2020
 Script for regridding SMAP L3E data
 """
-from datetime import datetime
-import numpy as np
 import os
-import warnings
 import xarray as xr
-import xtools
+import xtools as xt
 import sm_config as config
-import matplotlib.pyplot as plt
 
 product = "SMAP L3 Enhanced"
 sm_field = config.dict_product_fields[product]['sm_field']
 native_res = "9 km"
-output_dir = r"..\input_data\xr"
-in_dir = r"..\input_data\SPL3SMP_E_smap-L3E_09km_clipped_geographic_nc"
+output_dir = r"/Users/nina/Documents/GitHub/soil-moisture-sweden/test_output_data"
+f = r"/Volumes/TOSHIBA EXT/sm_backup/xr/smap-L3E-09km-subset-nofilter.nc"
 
-clean_weights = False
-test_plots = True
-method = 'nearest_s2d'
-export_ds = True
-regrid = False
+ds = xr.open_dataset(f)
+# filter out invalid data
+ds = ds.where((ds['retrieval_qual_flag'] == 0) | (ds['retrieval_qual_flag'] == 1) | (ds['retrieval_qual_flag'] == 8))
+ds = ds.where((ds[sm_field] >= 0) & (ds[sm_field] < 1))
 
-# all files in directory must be product nc files
-file_list = [os.path.join(in_dir, file) for file in os.listdir(in_dir)]
-
-# open multi file dataset
-ds = xtools.get_mf_dataset(file_list, product)
-
-if export_ds:
-    ds.to_netcdf(os.path.join(output_dir, "smap-L3E-09km-subset-nofilter.nc"))
-    print("smap-L3E-09km-subset-nofilter.nc complete")
-    print(ds)
-
-if regrid:
-    print("regridding")
-    dr = ds[sm_field]
-    dr_regrid = xtools.regrid(ds, sm_field, method=method)
-    print(dr_regrid)
-    dr_regrid.to_netcdf(os.path.join(output_dir, "smap_L3E_0-25-regrid.nc"))
-    print("smap_L3E_0-25-regrid.nc complete")
+dr_out = xt.regrid_multidate(ds, sm_field)
+dr_out.to_netcdf(os.path.join(output_dir, "smap-L3E_0-25-regrid.nc"))
