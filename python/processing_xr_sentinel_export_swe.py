796274093b56948c615d4022a4860c6e8010fd3d
ninadel <nina.del@gmail.com>
2020-07-29 15:19:31 -0600

    new file for exporting sentinel ts

---
+++ python/processing_xr_sentinel_export_swe.py
@@ -0,0 +1,19 @@
+import os
+import xarray as xr
+import xtools
+import sm_config as config
+
+product = 'Sentinel-1'
+output_dir = r"..\test_output_data\sentinel-1_regrid_ts"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+f = r"C:\Users\ninad\OneDrive - Lund University\Dokument\SM_Data_ReadOnly\satellite\0-25_regrid\Sentinel\sentinel_0-25-regrid-mask.nc"
+sm_field = config.dict_product_fields[product]['sm_field']
+
+ds = xr.open_dataset(f)
+print(ds)
+dr = ds[sm_field]
+print(dr)
+
+# def write_ts_quarter_deg(dr, output_dir, overwrite=False):
+xtools.write_ts_quarter_deg(dr, output_dir)
