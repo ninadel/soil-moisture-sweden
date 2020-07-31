cbbc04ce3bd3155ceebcda1425dd10284a3d4d95
ninadel <nina.del@gmail.com>
2020-07-30 20:42:32 -0600

    rewriting using concatenation

--- python/processing_xr_merge_gldas.py
+++ python/processing_xr_merge_gldas.py
@@ -1,12 +1,13 @@
 """
 Author: Nina del Rosario
 Date: 7/29/2020
-Script for merging SMOS-BEC data using xarray
+Script for merging GLDAS data using xarray
 """
 import os
 import xarray as xr
 import xtools as xt
 import sm_tools as tools
+import sm_config as config
 
 product = "GLDAS"
 output_dir = r"../test_output_data"
@@ -16,11 +17,13 @@ in_dir = r"D:\sm_backup\native\GLDAS_global"
 
 file_list = [os.path.join(in_dir, file) for file in os.listdir(in_dir)]
 
-# open files and subset
-ds = xt.get_mf_dataset(file_list, 'GLDAS')
-print(ds)
+concat_arrays = []
+for f in file_list:
+    ds = xr.open_dataset(f)
+    ds = ds['SoilMoi0_10cm_inst'].sel(
+        lat=slice(config.dict_extent_sweden['min_lat'], config.dict_extent_sweden['max_lat']),
+        lon=slice(config.dict_extent_sweden['min_lon'], config.dict_extent_sweden['max_lon']))
+    concat_arrays.append(ds)
 
-# export subset to nc
-ds.to_netcdf(os.path.join(output_dir, "gldas-subset-nofilter.nc"))
-print("gldas-subset-nofilter.nc")
-
+ds_concat = xr.concat(concat_arrays, dim="time")
+ds_concat.to_netcdf(os.path.join(output_dir, "gldas-subset-nofilter.nc"))
