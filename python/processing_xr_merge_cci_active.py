f2089766f851f02a75e0dde2e030c811abb7e93c
ninadel <nina.del@gmail.com>
2020-07-30 19:32:27 -0600

    new merge processing files

--- python/processing_xr_merge_cci_active.py
+++ python/processing_xr_merge_cci_active.py
@@ -1,26 +1,26 @@
 """
 Author: Nina del Rosario
 Date: 7/29/2020
-Script for merging SMOS-BEC data using xarray
+Script for merging CCI data using xarray
 """
 import os
 import xarray as xr
 import xtools as xt
 import sm_tools as tools
 
-product = "CCI Combined"
+product = "CCI Active"
 output_dir = r"../test_output_data"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
-in_dir = r"D:\sm_backup\native\cci-0.25deg_combined_global"
+in_dir = r"D:\sm_backup\native\cci-0.25deg_active_global"
 
 file_list = [os.path.join(in_dir, file) for file in os.listdir(in_dir)]
 
 # open files and subset
-ds = xt.get_mf_dataset(file_list, 'CCI Combined')
+ds = xt.get_mf_dataset(file_list, 'CCI Active')
 print(ds)
 
 # export subset to nc
-ds.to_netcdf(os.path.join(output_dir, "cci-combined-subset-nofilter.nc"))
-print("cci-combined-subset-nofilter.nc")
+ds.to_netcdf(os.path.join(output_dir, "cci-active-subset-nofilter.nc"))
+print("cci-active-subset-nofilter.nc")
 
--- python/processing_xr_merge_gldas.py
+++ python/processing_xr_merge_gldas.py
@@ -9,18 +9,18 @@ import xtools as xt
 import sm_tools as tools
 
 product = "GLDAS"
-output_dir = r"/Users/nina/Documents/GitHub/soil-moisture-sweden/test_output_data"
+output_dir = r"../test_output_data"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
-in_dir = r"/Volumes/TOSHIBA EXT/sm_backup/native/smos-bec-reprocessed-01km-euro/ASC"
+in_dir = r"D:\sm_backup\native\GLDAS_global"
 
 file_list = [os.path.join(in_dir, file) for file in os.listdir(in_dir)]
 
 # open files and subset
-ds = xt.get_mf_dataset(file_list, 'SMOS-BEC')
+ds = xt.get_mf_dataset(file_list, 'GLDAS')
 print(ds)
 
 # export subset to nc
-ds.to_netcdf(os.path.join(output_dir, "smos-bec_01km-subset-nofilter.nc"))
-print("smos-bec_01km-subset-nofilter.nc")
+ds.to_netcdf(os.path.join(output_dir, "gldas-subset-nofilter.nc"))
+print("gldas-subset-nofilter.nc")
 
