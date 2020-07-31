4e6d842a80871f115dea6371c6eb97d77d178af8
Nina Del Rosario <nina.del@gmail.com>
2020-07-30 19:08:27 -0600

    starting file for merging/subsetting GLDAS data

---
+++ python/processing_xr_merge_gldas.py
@@ -0,0 +1,26 @@
+"""
+Author: Nina del Rosario
+Date: 7/29/2020
+Script for merging SMOS-BEC data using xarray
+"""
+import os
+import xarray as xr
+import xtools as xt
+import sm_tools as tools
+
+product = "GLDAS"
+output_dir = r"/Users/nina/Documents/GitHub/soil-moisture-sweden/test_output_data"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+in_dir = r"/Volumes/TOSHIBA EXT/sm_backup/native/smos-bec-reprocessed-01km-euro/ASC"
+
+file_list = [os.path.join(in_dir, file) for file in os.listdir(in_dir)]
+
+# open files and subset
+ds = xt.get_mf_dataset(file_list, 'SMOS-BEC')
+print(ds)
+
+# export subset to nc
+ds.to_netcdf(os.path.join(output_dir, "smos-bec_01km-subset-nofilter.nc"))
+print("smos-bec_01km-subset-nofilter.nc")
+
