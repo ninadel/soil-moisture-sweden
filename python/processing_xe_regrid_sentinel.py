693a99ec85fb8fe74b9c29c54974d03d3b5556ea
Nina del Rosario <nina.del@gmail.com>
2020-07-31 16:34:13 -0600

    renaming

---
+++ python/processing_xe_regrid_sentinel.py
@@ -0,0 +1,135 @@
+"""
+Author: Nina del Rosario
+Date: 6/30/2020
+Script for regridding SMOS-IC data
+"""
+from datetime import datetime
+import numpy as np
+import os
+import warnings
+import xarray as xr
+import xtools as xt
+import xesmf as xe
+import sm_config as config
+import matplotlib.pyplot as plt
+
+product = "Sentinel-1"
+native_res = "1km"
+sm_field = config.dict_product_fields[product]['sm_field']
+clean_weights = False
+test_plots = True
+
+output_dir = r"../test_output_data/sentinel"
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+f = r"../test_output_data/sentinel/sentinel_01km_subset_validvalues.nc"
+# in_dir = r"/Volumes/TOSHIBA EXT/sm_backup/native/cgls-biopar-ssm-01km_europe"
+
+# file_list = [os.path.join(in_dir, file) for file in os.listdir(in_dir)]
+# print(file_list)
+# file = file_list[0]
+#
+ds = xr.open_dataset(f)
+# print(ds)
+# print(ds['ssm'].shape)
+# print(ds['ssm'][0,:,:])
+# print(ds[0,:,:])
+
+# print(ds['ssm'][0,:,:].shape)
+# print(ds['time'].shape)
+# print(len(ds['time']))
+# print(ds['time'][0].values)
+# print(ds)
+#
+ds_in = ds['ssm'][0, :, :]
+print(ds_in)
+print(ds_in.shape)
+ds_out = xr.Dataset({'lat': (['lat'], config.regrid_lat),
+                     'lon': (['lon'], config.regrid_lon)})
+nearest_reg = xe.Regridder(ds_in, ds_out, method="nearest_s2d", reuse_weights=True)
+bilinear_reg = xe.Regridder(ds_in, ds_out, method="bilinear", reuse_weights=True)
+
+concat_arrays = []
+
+for index, time in enumerate(ds['time'].values):
+    print(time)
+    dr_in = ds['ssm'][index,:,:]
+    # dr_in.plot()
+    # plt.show()
+    # plt.clf()
+    nearest_dr_out = nearest_reg(dr_in)
+    # nearest_dr_out.plot()
+    # plt.show()
+    # plt.clf()
+    output_shape = nearest_dr_out.shape
+    dr_regrid = nearest_dr_out.copy()
+    bilinear_dr_out = bilinear_reg(dr_in)
+    # print(bilinear_dr_out)
+    for i in range(0,output_shape[0]):
+        if bilinear_dr_out.values[i,:].mean() == 0:
+            dr_regrid.values[i,:] = np.nan
+    for i in range(0,output_shape[1]):
+        if bilinear_dr_out.values[:,i].mean() == 0:
+            dr_regrid.values[:,i] = np.nan
+    # dr_regrid.plot()
+    # plt.show()
+    # plt.clf()
+    # print(dr_regrid)
+    concat_arrays.append(dr_regrid)
+
+ds_concat = xr.concat(concat_arrays, dim="time")
+ds_concat.to_netcdf(os.path.join(output_dir, "sentinel_0-25-regrid-mask.nc"))
+
+
+# def regrid(ds_in, var, method='nearest_s2d', reuse=False, cleanup=False, mask=True):
+#     if 'latitude' in ds_in.coords:
+#         ds_in = ds_in.rename({'latitude': 'lat', 'longitude': 'lon'})
+#     if 'time' in ds_in.coords:
+#         ds_out = xr.Dataset({'lat': (['lat'], config.regrid_lat),
+#                              'lon': (['lon'], config.regrid_lon),
+#                             'time': (['time'], ds_in['time'].values)})
+#     else:
+#         ds_out = xr.Dataset({'lat': (['lat'], config.regrid_lat),
+#                              'lon': (['lon'], config.regrid_lon)})
+#     reg = xe.Regridder(ds_in, ds_out, method=method, reuse_weights=reuse)
+#     dr_in = ds_in[var]
+#     dr_out = reg(dr_in)
+#     if mask:
+#         # code for masking
+#         # do separate regrid with bilinear
+#         # use bilinear to mask results from nearest_s2d
+#         pass
+#     if cleanup:
+#         reg.clean_weight_file()  # clean-up
+#     return dr_out
+
+# for time in ds['time']
+    # extract 2D sm array
+    # extract time
+    # get nearests2d regrid
+    # get bilinear regrid
+    # extend with time
+
+# for time in range(0,len()):
+#     print(time.value)
+#     break
+
+# dr = ds['ssm']
+# dr_regrid = xt.regrid(ds, sm_field, method="nearest_s2d", reuse=True)
+# print("nearest_s2d")
+# print(dr_regrid)
+# dr_regrid_bl = xt.regrid(ds, sm_field, method="bilinear", reuse=True)
+# print("bilinear")
+# print(dr_regrid_bl)
+# dr_regrid[np.isnan(dr_regrid_bl)] = np.nan
+# print("mask")
+# print(dr_regrid)
+
+# # interpolate nan?
+# dr_regrid = xtools.regrid(ds, sm_field, method='nearest_s2d')
+#
+# # export regrid to nc
+# dr_regrid.to_netcdf(os.path.join(output_dir, "sentinel_0-25-regrid.nc"))
+# print("sentinel_0-25-regrid.nc complete")
+
--- python/processing_xr_merge_h115.py
+++ python/processing_xr_merge_h115.py
@@ -7,75 +7,22 @@ import numpy as np
 import pandas as pd
 import os
 
-def build_ds(array_dict, lat_grid, lon_grid, timestamp=None):
-    ds = xr.Dataset(
-        {
-            "sm": (
-                ("row", "col"),
-                array_dict["sm"],
-            ),
-            "sm_noise": (
-                ("row", "col"),
-                array_dict["sm_noise"],
-            ),
-            "conf_flag": (
-                ("row", "col"),
-                array_dict["conf_flag"],
-            ),
-            "corr_flag": (
-                ("row", "col"),
-                array_dict["corr_flag"],
-            ),
-            "proc_flag": (
-                ("row", "col"),
-                array_dict["proc_flag"],
-            ),
-            "ssf": (
-                ("row", "col"),
-                array_dict["ssf"],
-            ),
-        },
-        # coords={"lat": lat_grid, "lon": lon_grid, "row": rowNums, "col": colNums}
-        coords={
-            "lat": (("row", "col"), lat_grid),
-            "lon": (("row", "col"), lon_grid)
-        }
-    )
-    if timestamp is not None:
-        ds["time"] = timestamp
-        ds = ds.expand_dims("time").set_coords("time")
-    return ds
+in_dir = r"/Volumes/TOSHIBA EXT/sm_backup/xr/ascat_h115_reprocessed_AM_xr/timedim"
+output_dir = r"/Users/nina/Documents/GitHub/soil-moisture-sweden/test_output_data"
 
+file_list = [os.path.join(in_dir, file) for file in os.listdir(in_dir)]
 
-# dictionary which defines timeframes to analyze
-with open("dict_h115_swe_coords.json", "r") as f:
-    dict_h115_swe_coords = json.load(f)
+ds = xr.open_mfdataset(file_list, combine='by_coords')
+print(ds)
+ds.to_netcdf(os.path.join(output_dir, "ascat-h115_rebuild-subset-nofilter.nc"))
 
-input_dir = r"C:\Users\ninad\OneDrive - Lund University\Dokument\SM_Data_ReadOnly\csv\ascat-h115-ts_points_csv\overpass_data"
-output_dir = r"..\test_output_data\h115_reprocessed\timedim"
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
-template_f = r"C:\Users\ninad\OneDrive - Lund University\Dokument\SM_Data_ReadOnly\satellite\native_resolution\ASCAT-12-5\ascat_h115_swe_template.nc"
-ds = xr.open_dataset(template_f)
-ds_shape = ds['lat'].shape
-lat_array = ds['lat'].values
-lon_array = ds['lon'].values
-output_vars = ['sm', 'sm_noise', 'conf_flag', 'corr_flag', 'proc_flag', 'ssf']
-
-for filename in os.listdir(input_dir):
-    file = os.path.join(input_dir, filename)
-    arrays = xt.populate_arrays(dict_h115_swe_coords, ds_shape, file, output_vars)
-    sat = filename[3:4]
-    year = int(filename[5:9])
-    month = int(filename[10:12])
-    day = int(filename[13:15])
-    hour = int(filename[16:18])
-    minute = int(filename[19:21])
-    timestamp = datetime(year, month, day, hour, minute)
-    print(timestamp)
-    ds = build_ds(arrays, lat_array, lon_array, timestamp)
-    # ds = build_ds(arrays, lat_array, lon_array)
-    output_file = os.path.join(output_dir,
-                               "ascat-h115-12-5_reprocessed_sat{}_{}{:02d}{:02d}{:02d}{:02d}.nc".format(
-                                   sat,year,month,day,hour,minute))
-    ds.to_netcdf(output_file)
+# concat_arrays = []
+# for f in file_list:
+#     ds = xr.open_dataset(f)
+#     ds = ds['SoilMoi0_10cm_inst'].sel(
+#         lat=slice(config.dict_extent_sweden['min_lat'], config.dict_extent_sweden['max_lat']),
+#         lon=slice(config.dict_extent_sweden['min_lon'], config.dict_extent_sweden['max_lon']))
+#     concat_arrays.append(ds)
+#
+# ds_concat = xr.concat(concat_arrays, dim="time")
+# ds_concat.to_netcdf(os.path.join(output_dir, "gldas-subset-nofilter.nc"))
