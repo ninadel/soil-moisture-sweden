6315323359252a4dc942179fbea3261319b28bf3
Nina del Rosario <nina.del@gmail.com>
2020-07-31 22:14:57 -0600

    updating to include new regrid_multidate function

--- python/processing_xe_regrid_era5.py
+++ python/processing_xe_regrid_era5.py
@@ -1,36 +1,19 @@
 """
 Author: Nina del Rosario
 Date: 6/27/2020
-Script for exporting CSV files of TS within a bounding box around Sweden
+Script for regridding ERA5 file
 """
 
-# import netCDF4
-import datetime
-import warnings
 import xarray as xr
-try:
-    import xesmf as xe
-except:
-    warnings.warn("could not import xesmf. not windows compatible.")
-# import matplotlib
-import numpy
-import os
 import sm_config as config
-# import sm_tools as tools
+import xtools as xt
+import os
 
-f = r"../input_data/ERA5-Land/0-1_ERA5-Land_hourly.nc"
+product = "ERA5 0-1"
+sm_field = config.dict_product_fields[product]['sm_field']
+f = r"/Volumes/TOSHIBA EXT/sm_backup/native/ERA5-Land/hourly/0-1_ERA5-Land_hourly.nc"
 ds = xr.open_dataset(f)
-output_dir = r"../era5_ts"
+output_dir = r"../test_output_data"
 
-ds = ds.rename({'latitude': 'lat', 'longitude': 'lon'})
-dr = ds['swvl1']
-
-ds_out = xr.Dataset({'lat': (['lat'], config.regrid_lat),
-                     'lon': (['lon'], config.regrid_lon),
-                     'time': (['time'], ds['time'].values)
-                    })
-
-reg = xe.Regridder(ds, ds_out, 'nearest_s2d')
-dr_out = reg(dr)
-
-# EXPORT dr to .nc
+dr_out = xt.regrid_multidate(ds, sm_field)
+dr_out.to_netcdf(os.path.join(output_dir, "era-0-1_0-25-regrid.nc"))
