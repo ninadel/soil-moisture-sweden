6f4643908848098e0a50c8754da3f4e55c0dd439
Nina Del Rosario <nina.del@gmail.com>
2020-08-04 16:24:46 -0600

    updating merge script to combine all products

--- python/processing_merge_gridevaluation_metrics.py
+++ python/processing_merge_gridevaluation_metrics.py
@@ -1,19 +1,17 @@
 import os
 import pandas as pd
 
-input_dir = r"C:\git\soil-moisture-sweden\test_input_data\20200803222312 SMAP L4 ERA5 0-1 absolute - Copy"
-output_dir = r"C:\git\soil-moisture-sweden\test_output_data"
-print(os.listdir(input_dir))
+input_dir = r"..\analysis_output\ERA5 0-1 grid evaluation 20200804044015"
+output_dir = input_dir
 
 merged_metrics = None
 for filename in os.listdir(input_dir):
-    file = os.path.join(input_dir, filename)
-    df = pd.read_csv(file)
-    if merged_metrics is None:
-        merged_metrics = df
-    else:
-        merged_metrics = pd.concat([merged_metrics, df])
+    if filename[-4::] == ".csv":
+        file = os.path.join(input_dir, filename)
+        df = pd.read_csv(file)
+        if merged_metrics is None:
+            merged_metrics = df
+        else:
+            merged_metrics = pd.concat([merged_metrics, df])
 
-print(merged_metrics)
-print(merged_metrics.columns)
-merged_metrics.to_csv(os.path.join(output_dir, "20200803222312 SMAP L4 ERA5 0-1 absolute merged metrics.csv"))
+merged_metrics.to_csv(os.path.join(output_dir, "merged metrics.csv"), index=False)
