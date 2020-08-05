e6a835c7d6a7a03b37d47864e45e3fbf244ec1d6
ninadel <nina.del@gmail.com>
2020-08-05 13:09:49 -0600

    script complete, breaks for testing

--- python/sm_griddataexport.py
+++ python/sm_griddataexport.py
@@ -11,18 +11,21 @@ import sm_evaluation as evaluation
 import xarray as xr
 import pandas as pd
 import warnings
+from pytesmo import temporal_matching
+from pytesmo.time_series.anomaly import calc_anomaly
 from multiprocessing import Pool
 
-reference_dataset_name = 'ERA5 0-1'
-reference_file = config.dict_quarterdeg_files[reference_dataset_name]
-reference_dataset = xr.open_dataset(reference_file)
-anomaly_evaluation = [False, True]
-output_root = r"../test_output_data/grid_evaluation_data".format(reference_dataset_name,
-                                                                 datetime.now().strftime("%Y%m%d%H%M%S"))
+ref_dataset_name = 'ERA5 0-1'
+ref_file = config.dict_quarterdeg_files[ref_dataset_name]
+ref_dataset = xr.open_dataset(ref_file)
+start_date = datetime(2015,4,1)
+end_date = datetime(2018,23,59,59)
+output_root = r"../test_output_data/grid_evaluation_data"
+locations = config.dict_swe_gldas_points
 
 grid_dataexport_dict = {
     'locations': config.dict_swe_gldas_points,
-    'reference': (reference_dataset_name, reference_dataset),
+    'reference': (ref_dataset_name, ref_dataset),
     'evaluation': (None, None),
     'start_date': None,
     'end_date': None,
@@ -35,8 +38,42 @@ grid_dataexport_dict = {
 evaluation_datasets = ['SMAP L4', 'ASCAT 12.5 TS', 'SMAP L3 Enhanced', 'GLDAS', 'Sentinel-1', 'SMOS-BEC', 'SMOS-IC',
                        'SMAP L3', 'CCI Combined', 'CCI Passive', 'CCI Active']
 
-def grid_export_data(evaluation_dataset_name):
-    pass
+def grid_export_data(eval_dataset_name):
+    ref_ts_folder = os.path.join(output_root, eval_dataset_name, "{}_ts".format(ref_dataset_name))
+    eval_ts_folder = os.path.join(output_root, eval_dataset_name, "{}_ts".format(eval_dataset_name))
+    matched_folder = os.path.join(output_root, eval_dataset_name, "{}_{}-matched".format(ref_dataset_name,
+                                                                                       eval_dataset_name))
+    os.makedirs(ref_ts_folder)
+    os.mkdir(eval_ts_folder)
+    os.mkdir(matched_folder)
+    eval_dataset = xr.open_dataset(config.dict_quarterdeg_files[eval_dataset_name])
+    ref_sm_field = config.dict_product_fields[ref_dataset_name]['sm_field']
+    eval_sm_field = config.dict_product_fields[eval_dataset_name]['sm_field']
+    ref_sm_data = ref_dataset[ref_sm_field]
+    eval_sm_data = eval_dataset[eval_sm_field]
+    for loc, loc_data in locations.items():
+        lon = loc_data['longitude']
+        lat = loc_data['latitude']
+        ref_ts = ref_sm_data.sel(time=slice(start_date, end_date), lat=lat, lon=lon).to_pandas()
+        eval_ts = eval_sm_data.sel(time=slice(start_date, end_date), lat=lat, lon=lon).to_pandas()
+        ref_ts.rename('ref_sm', inplace=True)
+        eval_ts.rename('eval_ts', inplace=True)
+        ref_abs_ts = ref_ts.to_frame()
+        ref_anom_ts = calc_anomaly(ref_ts).to_frame()
+        eval_abs_ts = eval_ts.to_frame()
+        eval_anom_ts = calc_anomaly(eval_ts).to_frame()
+        matched_abs_data = temporal_matching.matching(ref_abs_ts, eval_abs_ts, window=1/24.)
+        matched_anom_data = temporal_matching.matching(ref_anom_ts, eval_anom_ts, window=1/24.)
+        ref_abs_ts.to_csv(ref_ts_folder,"{}_{}_abs_ts.csv".format(ref_dataset_name,loc))
+        ref_anom_ts.to_csv(ref_ts_folder,"{}_{}_anom_ts.csv".format(ref_dataset_name,loc))
+        eval_abs_ts.to_csv(eval_ts_folder,"{}_{}_abs_ts.csv".format(eval_dataset_name,loc))
+        eval_anom_ts.to_csv(eval_ts_folder,"{}_{}_anom_ts.csv".format(eval_dataset_name,loc))
+        matched_abs_data.to_csv(matched_folder,"{}_{}_{}_abs_matched.csv".format(ref_dataset_name,eval_dataset_name,
+                                                                                   loc))
+        matched_anom_data.to_csv(matched_folder,"{}_{}_{}_anom_matched.csv".format(ref_dataset_name,eval_dataset_name,
+                                                                                   loc))
+        break
+    break
 
 if __name__ == '__main__':
     with Pool(5) as p:
