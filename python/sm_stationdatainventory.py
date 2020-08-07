73baea660e39dae2d7a0cbfbf9597f0b8d0df3bf
Nina Del Rosario <nina.del@gmail.com>
2020-08-07 17:37:35 -0600

    adding ascat inventory (working)

--- python/sm_stationdatainventory.py
+++ python/sm_stationdatainventory.py
@@ -14,7 +14,7 @@ import pandas as pd
 import warnings
 from multiprocessing import Pool
 
-output_root = "../test_output/_data"
+output_root = "../test_output/inventory_data"
 
 evaluation_datasets = ['SMAP L4', 'ASCAT 12.5 TS', 'SMAP L3 Enhanced', 'GLDAS', 'ERA5 0-1', 'ERA5 0-25', 'Sentinel-1',
                        'SMOS-BEC', 'SMOS-IC', 'SMAP L3', 'CCI Combined', 'CCI Passive', 'CCI Active']
@@ -24,28 +24,116 @@ icos_readers = tools.get_icos_readers(config.icos_input_dir)
 ismn_readers = tools.get_ismn_readers(config.ismn_input_dir)
 evaluation_stations = icos_readers + ismn_readers
 
-def inventory_ascat_h115(df):
-    pass
+def get_station_inventory(dataset_name, start_date, end_date, station):
+    def get_inventory_dict(filter, df):
+        inventory_dict = {'dataset': dataset_name, 'network': station.network, 'station': station.station,
+                          'lat': station.latitude, 'lon': station.longitude, 'filter': filter}
+        timefilter_counts = tools.split_by_timeframe(df, years=(2015, 2018),
+                                                     ignore=[(2015, "winter", None), (2015, None, 1),
+                                                             (2015, None, 2), (2015, None, 3)])[1]
+        inventory_dict.update(timefilter_counts)
+        return inventory_dict
+    lon = station.longitude
+    lat = station.latitude
+    f = config.dict_native_files[dataset_name]
+    ds = xr.open_dataset(f)
+    sm_field = config.dict_product_fields[dataset_name]['sm_field']
+    sm = ds[sm_field]
+    if dataset_name == "ASCAT 12.5 TS":
+        row = tools.find_nearest(config.dict_h115_coords['lat'], lat)[0]
+        col = tools.find_nearest(config.dict_h115_coords[str(row)], lon)[0]
+        df = sm.sel(time=slice(start_date, end_date), row=row, col=col).to_pandas().dropna()
+        inventory_dict = get_inventory_dict('DropNA', df)
+        inventory_cols = list(inventory_dict.keys())
+        inventory_df = pd.DataFrame(columns=inventory_cols)
+        inventory_df = inventory_df.append(inventory_dict, ignore_index=True)
+        # noise filters
+        noise = ds['sm_noise']
+        df = noise.sel(time=slice(start_date, end_date), row=row, col=col).to_pandas().dropna()
+        noise_filter = df[df < 50]
+        inventory_dict = get_inventory_dict('noise_below50', noise_filter)
+        inventory_df = inventory_df.append(inventory_dict, ignore_index=True)
+        noise_filter = df[df < 20]
+        inventory_dict = get_inventory_dict('noise_below20', noise_filter)
+        inventory_df = inventory_df.append(inventory_dict, ignore_index=True)
+        noise_filter = df[df < 10]
+        inventory_dict = get_inventory_dict('noise_below10', noise_filter)
+        inventory_df = inventory_df.append(inventory_dict, ignore_index=True)
+        noise_filter = df[df < 5]
+        inventory_dict = get_inventory_dict('noise_below05', noise_filter)
+        inventory_df = inventory_df.append(inventory_dict, ignore_index=True)
+        # conf_flag filters
+        conf_flag = ds['conf_flag']
+        df = conf_flag.sel(time=slice(start_date, end_date), row=row, col=col).to_pandas().dropna()
+        conf_values = df.unique()
+        for value in conf_values:
+            conf_filter = df[df == value]
+            inventory_dict = get_inventory_dict('confflag_{}'.format(value), conf_filter)
+            inventory_df = inventory_df.append(inventory_dict, ignore_index=True)
+        # corr_flag filters
+        corr_flag = ds['corr_flag']
+        df = corr_flag.sel(time=slice(start_date, end_date), row=row, col=col).to_pandas().dropna()
+        corr_values = df.unique()
+        for value in corr_values:
+            corr_filter = df[df == value]
+            inventory_dict = get_inventory_dict('corrflag_{}'.format(value), corr_filter)
+            inventory_df = inventory_df.append(inventory_dict, ignore_index=True)
+        # proc_flag filters
+        proc_flag = ds['proc_flag']
+        df = proc_flag.sel(time=slice(start_date, end_date), row=row, col=col).to_pandas().dropna()
+        proc_values = df.unique()
+        for value in proc_values:
+            proc_filter = df[df == value]
+            inventory_dict = get_inventory_dict('procflag_{}'.format(value), proc_filter)
+            inventory_df = inventory_df.append(inventory_dict, ignore_index=True)
+        # ssf filters
+        ssf = ds['ssf']
+        df = ssf.sel(time=slice(start_date, end_date), row=row, col=col).to_pandas().dropna()
+        ssf_values = df.unique()
+        for value in ssf_values:
+            ssf_filter = df[df == value]
+            inventory_dict = get_inventory_dict('ssf_{}'.format(value), ssf_filter)
+            inventory_df = inventory_df.append(inventory_dict, ignore_index=True)
+        return inventory_df
+    elif dataset_name == 'CCI Active':
+        pass
+    elif dataset_name == 'CCI Passive':
+        pass
+    elif dataset_name == 'CCI Combined':
+        pass
+    elif dataset_name == 'ERA 0-1':
+        pass
+    elif dataset_name == 'ERA 0-25':
+        pass
+    elif dataset_name == 'GLDAS':
+        pass
+    elif dataset_name == 'SMAP L3':
+        pass
+    elif dataset_name == 'SMAP L3 Enhanced':
+        pass
+    elif dataset_name == 'SMAP L4':
+        pass
+    elif dataset_name == 'Sentinel-1':
+        pass
+    elif dataset_name == 'SMOS-BEC':
+        pass
+    elif dataset_name == 'SMOS-IC':
+        pass
 
-def inventory_data():
+def get_dataset_station_inventory(dataset):
+    output_folder = os.path.join(output_root, dataset)
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
     inventory_df = None
-    for station in icos_readers:
-        station_name = station.station
-        network_name = station.network
-        pass
-    for station in ismn_readers:
-        station_name = station.station
-        network_name = station.network
-        pass
-    # station_data_dir = os.path.join(output_root, "station_data")
-    # dataset_data_dir = os.path.join(output_root, dataset)
     for station in evaluation_stations:
-        station_name = station.station
-        network_name = station.network
-        station_data_file = os.path.join(station_data_dir, '{}_{}.csv'.format(network_name, station_name)
-        if not os.path.exists(station_data_file):
-            pass
-        evaluation_data_filename = "{}_{}_{}.csv".format(dataset, network_name, station_name)
+        station_inventory = get_station_inventory(dataset, start_date=datetime(2015,4,1),
+                                                  end_date=datetime(2018, 12, 31, 23, 59, 59), station=station)
+        if inventory_df is None:
+            inventory_df = station_inventory
+        else:
+            inventory_df = pd.concat([inventory_df, station_inventory])
+    inventory_df.to_csv(os.path.join(output_folder, "{} inventory.csv".format(dataset)), index=False)
+    return inventory_df
 
 
 # icos_readers = tools.get_icos_readers(config.icos_input_dir)
@@ -76,6 +164,8 @@ def inventory_data():
 #
 # icos_results = evaluation.evaluate_network_product(icos_readers, 'ASCAT 12.5 TS', startdate=datetime, enddate=enddate)
 
-if __name__ == '__main__':
-    with Pool(5) as p:
-        p.map(export_station_data, evaluation_datasets)
+# if __name__ == '__main__':
+#     with Pool(5) as p:
+#         p.map(export_station_data, evaluation_datasets)
+
+test = get_dataset_station_inventory('ASCAT 12.5 TS')
