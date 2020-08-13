439126baf8f96747cd1925c4469afbcab18feca1
ninadel <nina.del@gmail.com>
2020-08-12 21:48:52 -0600

    fixing - adding try except

--- python/sm_stationdataexport.py
+++ python/sm_stationdataexport.py
@@ -14,7 +14,7 @@ import pandas as pd
 import warnings
 from multiprocessing import Pool
 
-output_root = "../test_output/csv_station_data"
+output_root = "../test_output_data/csv_station_data"
 
 evaluation_datasets = ['SMAP L4', 'ASCAT 12.5 TS', 'SMAP L3 Enhanced', 'GLDAS', 'ERA5 0-1', 'ERA5 0-25', 'Sentinel-1',
                        'SMOS-BEC', 'SMOS-IC', 'SMAP L3', 'CCI Combined', 'CCI Passive', 'CCI Active']
@@ -24,26 +24,50 @@ ismn_readers = tools.get_ismn_readers(config.ismn_input_dir)
 evaluation_stations = icos_readers + ismn_readers
 
 def export_station_data(dataset):
+    print(dataset)
     dataset_data_dir = os.path.join(output_root, dataset)
     if not os.path.exists(dataset_data_dir):
         os.makedirs(dataset_data_dir)
     xr_file = config.dict_native_files[dataset]
-    df = xr.open_dataset(xr_file)
+    ds = xr.open_dataset(xr_file)
     for station in evaluation_stations:
+        drs = []
         network_name = station.network
         station_name = station.station
-        station_name.replace(".", "-")
+        print(network_name, station_name)
+        station_name = station_name.replace(".", "-")
         lat = station.latitude
         lon = station.longitude
-    if dataset == "ASCAT 12.5 TS":
-        row = tools.find_nearest(config.dict_h115_coords['lat'], lat)[0]
-        col = tools.find_nearest(config.dict_h115_coords[str(row)], lon)[0]
-        df = df.sel(row=row, col=col).to_pandas()
-    else:
-        df = df.sel(lat=lat, lon=lon).to_pandas()
-    df.to_csv(os.path.join(dataset_data_dir, "{}_{}.csv".format(network_name, station_name)))
+        if dataset == "ASCAT 12.5 TS":
+            row = tools.find_nearest(config.dict_h115_coords['lat'], lat)[0]
+            col = tools.find_nearest(config.dict_h115_coords[str(row)], lon)[0]
+            for var in ds.data_vars:
+                try:
+                    dr = ds[var].sel(row=row, col=col)
+                    drs.append(dr)
+                except:
+                    print("did not append {}".format(var))
+        elif dataset in ['ERA5 0-1', 'ERA5 0-25']:
+            for var in ds.data_vars:
+                try:
+                    dr = ds[var].sel(latitude=lat, longitude=lon, method='nearest')
+                    drs.append(dr)
+                except:
+                    print("did not append {}".format(var))
+        else:
+            for var in ds.data_vars:
+                try:
+                    dr = ds[var].sel(lat=lat, lon=lon, method='nearest')
+                    drs.append(dr)
+                except:
+                    print("did not append {}".format(var))
+        df = xr.merge(drs).to_dataframe()
+        df.to_csv(os.path.join(dataset_data_dir, "{}_{}.csv".format(network_name, station_name)))
 
 
-if __name__ == '__main__':
-    with Pool(5) as p:
-        p.map(export_station_data, evaluation_datasets)
+for dataset in evaluation_datasets:
+    export_station_data(dataset)
+
+# if __name__ == '__main__':
+#     with Pool(5) as p:
+#         p.map(export_station_data, evaluation_datasets)
