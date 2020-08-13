8c2fcfc0cc43c71832717302609d412396c2d70f
Nina Del Rosario <nina.del@gmail.com>
2020-08-12 19:03:23 -0600

    changing metrics function reference, cleaning up

--- python/sm_xr_evaluation.py
+++ python/sm_xr_evaluation.py
@@ -5,7 +5,7 @@ Script for evaluating SM datasets
 """
 from datetime import datetime
 import os
-import pandas
+import pandas as pd
 import warnings
 
 from pytesmo import metrics, temporal_matching
@@ -53,11 +53,11 @@ def evaluate_gridcell_xr(evaluation_dict, lon, lat):
     data['matched_data'] = matched_data
     data['ref_ts'] = ref_ts
     data['eval_ts'] = eval_ts
-    metrics['all'] = get_evaluation_metrics_xr(matched_data, anomaly)
+    metrics['all'] = tools.get_metrics(data=matched_data, anomaly=anomaly, return_dict=True)
     if evaluate_timeframes:
         timeframe_data_dict, timeframe_counts = tools.split_by_timeframe(matched_data)
         for timeframe, timeframe_data in timeframe_data_dict.items():
-            metrics[timeframe] = get_evaluation_metrics_xr(timeframe_data, anomaly)
+            metrics[timeframe] = tools.get_metrics(data=timeframe_data, anomaly=anomaly, return_dict=True)
     return metrics, data
 
 
@@ -99,7 +99,7 @@ def evaluate_insitu_network_xr(evaluation_dict):
 # function for evaluating a set of grid cells in a region
 def evaluate_grid_xr(evaluation_dict):
     def get_new_grid_metrics_df():
-        metrics_df = pandas.DataFrame(columns=['ref_dataset', 'eval_dataset', 'timefilter', 'anomaly', 'loc',
+        metrics_df = pd.DataFrame(columns=['ref_dataset', 'eval_dataset', 'timefilter', 'anomaly', 'loc',
                                                'lon', 'lat', 'veg_class', 'pearson_r', 'pearson_r_p-value', 'bias',
                                                'rmsd', 'ubrmsd', 'n', 'pearson_sig'])
         return metrics_df
@@ -177,228 +177,3 @@ def evaluate_grid_xr(evaluation_dict):
                           index=False)
     return metrics_dict
 
-
-def get_evaluation_metrics_xr(evaluation_data, anomaly=False, sig_level=0.05):
-    metrics_dict = {'pearson_r': None, 'pearson_r_p-value': None, 'bias': None, 'rmsd': None, 'ubrmsd': None, 'n': None,
-                    'pearson_sig': None}
-
-    columns = evaluation_data.columns
-    x = evaluation_data[columns[0]].values
-    y = evaluation_data[columns[1]].values
-
-    metrics_dict['n'] = evaluation_data.shape[0]
-    try:
-        if anomaly is False:
-            metrics_dict['bias'] = metrics.bias(x, y)
-            metrics_dict['rmsd'] = metrics.rmsd(x, y)
-            metrics_dict['ubrmsd'] = metrics.ubrmsd(x, y)
-    except:
-        message = "could not calculate bias, rmsd, ubrmsd"
-        warnings.warn(message)
-
-    try:
-        metrics_dict['pearson_r'] = metrics.pearsonr(x, y)[0]
-        metrics_dict['pearson_r_p-value'] = metrics.pearsonr(x, y)[1]
-        metrics_dict['pearson_sig'] = metrics.pearsonr(x, y)[1] < sig_level
-    except:
-        metrics_dict['pearson_r'] = None
-        metrics_dict['pearson_r_p-value'] = None
-
-    return metrics_dict
-
-
-def evaluate_shuffle(references, products, output_folder, startdate=datetime(2015, 4, 1), enddate=datetime(2018, 12, 31, 23, 59),
-             export_ts=True, evaluate_timeframes=True, anomaly=False, metrics_df=None):
-    """""
-    Function to compare soil moisture product to reference data
-    Reference data supported: ICOS and ISMN (GLDAS forthcoming)
-
-    Parameters
-    ----------
-    references: list of ICOS or ISMN time series objects (adding GLDAS soon)
-    products:   dictionary of product(s) to analyze, examples:
-                evaluation_dict = {"ASCAT 12.5 TS": True}
-                see product_parameters_dict in sm_config.py for dataset names and to set dataset parameters
-    output_folder: directory where metrics and ts data will be saved
-    startdate:  datetime used to filter product data to be analyzed (default 4/1/2015)
-    enddate:    datetime used to filter product data to be analyzed (default 12/31/2018 23:59)
-    export_ts: whether ts data (reference, product, and matched) should be exported as csv files to output_folder
-    evaluate_timeframes: boolean, if true config.years_timeframes and config.seasons_timeframes will be evaluated
-    metrics_df: df of metrics. if None, new metrics_df will be created. otherwise existing metrics_df will be extended.
-    """""
-    log_file = os.path.join(output_folder, "analysis_log.txt")
-    data_output_folder = os.path.join(output_folder, "data_output")
-
-    # in case analysis fails, get an empty metrics row to indicate that something failed
-    def get_empty_metrics_df():
-        empty_df = pandas.DataFrame([[network_name, station_name, product_str, timeframe, anomaly, n, None, None,
-                                      None, None, None]], columns=config.metrics_df_columns)
-        return empty_df
-
-    if not os.path.exists(data_output_folder):
-        os.mkdir(data_output_folder)
-    if metrics_df is None:
-        metrics_df = pandas.DataFrame(columns=config.metrics_df_columns)
-    else:
-        metrics_df = metrics_df
-    product_list = tools.get_product_list(products)
-    if anomaly:
-        anomaly_str = "anomaly"
-    else:
-        anomaly_str = "absolute"
-    matched_data_dict = {}
-    tools.write_log(
-        log_file,
-        "*** EVALUATION STARTING: {} - {}, evaluate_timeframes: {}, anomaly: {} ***".format(
-            startdate, enddate, evaluate_timeframes, anomaly))
-    for product in product_list:
-        tools.write_log(log_file, "*** STARTING {} ({}) ANALYSIS ***".format(product, anomaly_str))
-        timeframe = "all-dates"
-        product_str = product.replace(' ', '-')
-        product_reader = tools.get_product_reader(product, config.dict_product_inputs[product])
-        # product_sm_col = config.dict_product_fields[product]['sm_field']
-        matched_data_dict = {}
-        for station in references:
-            station_name = station.station
-            network_name = station.network
-            ref_data_str = 'ref_data_{}_{}_{}_{}.csv'.format(network_name, station_name, timeframe, anomaly_str)
-            product_data_str = 'product_data_{}_{}_{}_{}_{}.csv'.format(product_str, network_name, station_name,
-                                                                        timeframe, anomaly_str)
-            matched_data_str = 'station_matched_data_{}_{}_{}_{}_{}.csv'.format(product_str, network_name,
-                                                                                station_name, timeframe, anomaly_str)
-            # scaled_data_str = 'station_scaled_data_{}_{}_{}_{}_{}.csv'.format(product_str, network_name,
-            #                                                                   station_name, timeframe, anomaly_str)
-            if network_name not in matched_data_dict.keys():
-                matched_data_dict[network_name] = pandas.DataFrame()
-            tools.write_log(log_file, "*** analyzing {} x {} {} ({}) ***".format(product, network_name,
-                                                                                 station_name, anomaly_str))
-            ref_data = tools.get_ref_data(station, anomaly=anomaly)
-            tools.write_log(log_file, 'ref_data.shape: {}'.format(ref_data.shape))
-            if export_ts:
-                ref_data.to_csv(os.path.join(data_output_folder, ref_data_str))
-            # ref_sm_col = tools.get_ref_data(station)[1]
-            ref_data.rename('ref_sm', inplace=True)
-            product_data = tools.get_product_data(lon=station.longitude, lat=station.latitude, product=product,
-                                                  reader=product_reader, anomaly=anomaly, station=station)
-            product_data.rename('product_sm', inplace=True)
-            tools.write_log(log_file, 'product_data.shape: {}'.format(product_data.shape))
-            product_data = product_data.loc[startdate:enddate]
-            tools.write_log(log_file, 'product_data.shape (with date filter): {}'.format(product_data.shape))
-            product_data = tools.get_timeshifted_data(product, product_data)
-            if export_ts:
-                product_data.to_csv(os.path.join(data_output_folder, product_data_str))
-            matched_data = pandas.DataFrame()
-            if product_data.shape[0] > 0:
-                matched_data = temporal_matching.matching(product_data, ref_data, window=1 / 24.)
-                n = matched_data.shape[0]
-                if export_ts:
-                    matched_data.to_csv(os.path.join(data_output_folder, matched_data_str))
-                network_matched_data = matched_data_dict[network_name]
-                matched_data_dict[network_name] = pandas.concat([network_matched_data, matched_data])
-                tools.write_log(log_file, '{} matched data shape: {}'.format(
-                    network_name, matched_data_dict[network_name].shape))
-                tools.write_log(log_file, 'matched_data.shape: {}'.format(matched_data.shape))
-            # insert scaling conditional here
-            else:
-                n = 0
-                tools.write_log(log_file, 'insufficient data for matching: {}'.format(product))
-            try:
-                metric_values = tools.get_metrics(matched_data, 'product_sm', 'ref_sm', anomaly)
-                station_metrics_df = pandas.DataFrame([[network_name, station_name, product_str, timeframe,
-                                                        anomaly, n] + metric_values], columns=config.metrics_df_columns)
-                tools.write_log(log_file, str(station_metrics_df))
-                metrics_df = pandas.concat([metrics_df, station_metrics_df])
-                tools.write_log(log_file, '{} x {} {} ({}) analysis complete'.format(product, network_name,
-                                                                                     station_name, anomaly_str))
-                tools.write_log(log_file, '')
-            except:
-                empty_metrics_df = get_empty_metrics_df()
-                metrics_df = pandas.concat([metrics_df, empty_metrics_df])
-                message = "Could not calculate metrics. Skipping to next station."
-                warnings.warn(message)
-                tools.write_log(log_file, message)
-                # write break here?
-        # network level analysis
-        tools.write_log(log_file, '*** analyzing networks ({}) ***'.format(anomaly_str))
-        for network, network_matched_data in matched_data_dict.items():
-            station_name = "All"
-            # network_matched_data.dropna
-            data_output_str = 'network_{}_{}_{}_{}_matched_data.csv'.format(
-                product, network_name, timeframe, anomaly_str)
-            if export_ts:
-                network_matched_data.to_csv(os.path.join(data_output_folder, data_output_str))
-            tools.write_log(log_file, '*** analyzing {} ({}) ***'.format(network, anomaly_str))
-            try:
-                metric_values = tools.get_metrics(network_matched_data, 'product_sm', 'ref_sm', anomaly)
-                n = network_matched_data.shape[0]
-                network_metrics_df = pandas.DataFrame(
-                    [[network_name, station_name, product_str, timeframe, anomaly, n] + metric_values],
-                    columns=config.metrics_df_columns)
-                tools.write_log(log_file, str(network_metrics_df))
-                metrics_df = pandas.concat([metrics_df, network_metrics_df])
-            except:
-                empty_metrics_df = get_empty_metrics_df()
-                metrics_df = pandas.concat([metrics_df, empty_metrics_df])
-                message = "Could not calculate network metrics."
-                warnings.warn(message)
-                tools.write_log(log_file, message)
-                # write break here?
-            if evaluate_timeframes:
-                if len(config.year_timeframes) > 0:
-                    for year in config.year_timeframes:
-                        timeframe = year
-                        data_output_str = 'network_{}_{}_{}_matched_data.csv'.format(network_name, timeframe,
-                                                                                     anomaly_str)
-                        tools.write_log(log_file, '*** analyzing {} {} for {} ({}) ***'.format(network, timeframe,
-                                                                                               product, anomaly_str))
-                        tf_network_matched_data = tools.timefilter_data(network_matched_data, year_filter=year)
-                        if export_ts:
-                            tf_network_matched_data.to_csv(os.path.join(data_output_folder, data_output_str))
-                        tools.write_log(log_file, '{} {} matched data shape: {}'.format(network_name, timeframe,
-                                                                                        tf_network_matched_data.shape))
-                        try:
-                            metric_values = tools.get_metrics(tf_network_matched_data, 'product_sm', 'ref_sm', anomaly)
-                            n = tf_network_matched_data.shape[0]
-                            tf_network_metrics_df = pandas.DataFrame(
-                                [[network_name, "All", product_str, timeframe, anomaly, n] + metric_values],
-                                columns=config.metrics_df_columns)
-                            tools.write_log(log_file, str(tf_network_metrics_df))
-                            metrics_df = pandas.concat([metrics_df, tf_network_metrics_df])
-                        except:
-                            empty_metrics_df = get_empty_metrics_df()
-                            metrics_df = pandas.concat([metrics_df, empty_metrics_df])
-                            message = "Could not calculate timeframe metrics."
-                            warnings.warn(message)
-                            tools.write_log(log_file, message)
-                            # write break here?
-                if len(config.season_timeframes) > 0:
-                    for season in config.season_timeframes:
-                        timeframe = season
-                        data_output_str = 'network_{}_{}_{}_matched_data.csv'.format(network_name, timeframe,
-                                                                                     anomaly_str)
-                        tools.write_log(log_file, '*** analyzing {} {} for {} ***'.format(network, timeframe, product))
-                        tf_network_matched_data = tools.timefilter_data(network_matched_data,
-                                                                               season_filter=season)
-                        if export_ts:
-                            tf_network_matched_data.to_csv(os.path.join(data_output_folder, data_output_str))
-                        tools.write_log(log_file, '{} {} matched data shape: {}'.format(network_name, timeframe,
-                                                                                        tf_network_matched_data.shape))
-                        try:
-                            metric_values = tools.get_metrics(tf_network_matched_data, 'product_sm', 'ref_sm', anomaly)
-                            n = tf_network_matched_data.shape[0]
-                            tf_network_metrics_df = pandas.DataFrame(
-                                [[network_name, "All", product_str, timeframe, anomaly, n] + metric_values],
-                                columns=config.metrics_df_columns)
-                            tools.write_log(log_file, str(tf_network_metrics_df))
-                            metrics_df = pandas.concat([metrics_df, tf_network_metrics_df])
-                        except:
-                            empty_metrics_df = get_empty_metrics_df()
-                            metrics_df = pandas.concat([metrics_df, empty_metrics_df])
-                            message = "Could not calculate timeframe metrics."
-                            warnings.warn(message)
-                            tools.write_log(log_file, message)
-                            # write break here?
-        tools.write_log(log_file, '*** {} ({}) ANALYSIS COMPLETE ***'.format(product, anomaly_str))
-        tools.write_log(log_file, '')
-        metrics_df.reset_index(drop=True, inplace=True)
-    return metrics_df
