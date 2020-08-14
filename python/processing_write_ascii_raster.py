ec8dfc2c6c6e5ddd705c600d2e5e426f54144c27
Nina Del Rosario <nina.del@gmail.com>
2020-08-13 20:13:48 -0600

    updated - pearson R working

--- python/processing_write_ascii_raster.py
+++ python/processing_write_ascii_raster.py
@@ -1,73 +1,68 @@
+import pandas as pd
 import os
-output_root = r"../test_output_folder"
-ncols = 52
-nrows = 55
-# 11.375, 52 columns
-# 55.125, 55 rows
-ncols = 52
-nrows = 55
-xllcorner = 11.25
-yllcorner = 55.25
-cellsize = 0.25
-NODATA_value = -9999
+
+def write_grid_ascii_raster(metric_file):
+    def get_quarter_grid_coord_index(lat, lon):
+        lon0 = 11.375
+        lat0 = 55.375
+        lats = [lat0 + (0.25 * i) for i in range(0, nrows)]
+        lats.sort(reverse=True)
+        lons = [lon0 + (0.25 * i) for i in range(0, ncols)]
+        lati = lats.index(lat)
+        loni = lons.index(lon)
+        index = (lati * ncols) + loni
+        return index
+    def start_ascii_file():
+        out_f = os.path.join(output_folder, "grideval_{}_{}_{}_{}.asc".format(dataset, timefilter, anomaly_str, metric))
+        asciif = open(out_f,"w")
+        asciif.write("ncols {}\n".format(ncols))
+        asciif.write("nrows {}\n".format(nrows))
+        asciif.write("xllcorner     {}\n".format(xllcorner))
+        asciif.write("yllcorner     {}\n".format(yllcorner))
+        asciif.write("cellsize      {}\n".format(cellsize))
+        asciif.write("NODATA_value  {}\n".format(NODATA_value))
+        return asciif
+    ncols = 52
+    nrows = 55
+    output_root = r"../test_output_data/grid_rasters"
+    filename = os.path.split(metric_file)[1]
+    split_filename = filename.split(" ")
+    dataset = split_filename[0]
+    timefilter = split_filename[-2]
+    anomaly_str = filename.split(" ")[-3]
+    output_folder = os.path.join(output_root, dataset)
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+    # 11.375, 52 columns
+    # 55.125, 55 rows
+    xllcorner = 11.25
+    yllcorner = 55.25
+    cellsize = 0.25
+    NODATA_value = -9999
+    metrics = ["pearson_r"]
+    # metrics = ["pearson_r", "bias", "ubrmsd"]
+    # metrics = ["pearson_r", "pearson_r_p-value", "bias", "rmsd", "ubrmsd"]
+    timefilters = ["all"]
+    df = pd.read_csv(metric_file)
+    print(df)
+    for metric in metrics:
+        asciif = start_ascii_file()
+        values = [NODATA_value] * (nrows * ncols)
+        if metric == "pearson_r":
+            df = df[df["pearson_r_p-value"] < 0.05]
+            print(df)
+        for index, row in df.iterrows():
+            lat = row['lat']
+            lon = row['lon']
+            value = row[metric]
+            gridi = get_quarter_grid_coord_index(lat=lat, lon=lon)
+            values[gridi] = value
+        for value in values:
+            asciif.write("{} ".format(value))
+        asciif.close()
 
 
-def get_coord_index(lat, lon):
-    lon0 = 11.375
-    lat0 = 55.375
-    lats = [lat0 + (0.25 * i) for i in range(0, nrows)]
-    lats.sort(reverse=True)
-    lons = [lon0 + (0.25 * i) for i in range(0, ncols)]
-    lati = lats.index(lat)
-    loni = lons.index(lon)
-    index = (lati * ncols) + loni
-    return index
-
-# ascii_dict = {}
-# counter = 0
-# for lat in lats:
-#     for lon in lons:
-#         ascii_dict[(lat,lon)] = counter
-#         counter += 1
-#
-# print(ascii_dict)
-# print(len(ascii_dict.keys()))
-
-# ncols         52
-# nrows         55
-# xllcorner     11.25
-# yllcorner     55.25
-# cellsize      0.25
-# NODATA_value  -9999
-
-# values = [-9999] * nrows * ncols
-# print(len(values))
-# print(get_coord_index(55.375, 11.375))
-# print(get_coord_index(68.875, 11.375))
-# print(get_coord_index(68.625, 11.375))
-# print(get_coord_index(68.625, 11.625))
-
-dataset_name = "GLDAS"
-metrics = ["pearson_r",	"pearson_r_p-value", "bias", "rmsd", "ubrmsd"]
-in_file = r"\\DESKTOP-J6H9VE7\SM_Data_ReadOnly\grid_evaluation_data\GLDAS ERA5 0-1 absolute\GLDAS ERA5 0-1 absolute all metrics.csv"
-
-
-out_filename = ""
-f = (os.path.join(output_root, "{}_{}.asc".format(dataset_name, metric)))
-asciif=open(f,"w")
-asciif.write("ncols {}\n".format(ncols))
-asciif.write("nrows {}\n".format(nrows))
-asciif.write("xllcorner     {}\n".format(xllcorner))
-asciif.write("yllcorner     {}\n".format(yllcorner))
-asciif.write("cellsize      {}\n".format(cellsize))
-asciif.write("NODATA_value  {}\n".format(NODATA_value))
-# for i in list, write "value " (without linebreak)
-# test by overlaying with arcmap generated file
-# for each metrics file
-    # open table
-    # get evaluation dataset name
-    # get timefilter
-    # filter table to significnt values
-    # for each metric column
-        # write data
-        # save to asc file: e.g. dataset_{pearsonr/bias/ubRMSE}_timeframe.asc
+in_dir = r"..\input_data\grid_evaluation_data\all_metrics"
+files = [os.path.join(in_dir, filename) for filename in os.listdir(in_dir)]
+for file in files:
+    write_grid_ascii_raster(file)
